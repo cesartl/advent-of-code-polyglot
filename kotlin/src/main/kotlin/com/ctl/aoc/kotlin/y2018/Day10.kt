ed128cf4ad0fc27dd8e93bf06654a075117e4397
Cesar TL <cesartronlozai@convendia.com>
2018-12-10 07:41:46 +0000

    day 10 tweaks

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2018/Day10.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2018/Day10.kt
@@ -15,18 +15,18 @@ object Day10 {
     fun pointsToMap(points: Sequence<Point>): MutableMap<Int, MutableMap<Int, List<Point>>> {
         val map = mutableMapOf<Int, MutableMap<Int, List<Point>>>()
         points.forEach {
-            val row = map.computeIfAbsent(it.x) { mutableMapOf() }
-            row.merge(it.y, listOf(it)){ l, x -> l + x}
+            val row = map.computeIfAbsent(it.y) { mutableMapOf() }
+            row.merge(it.x, listOf(it)) { l, x -> l + x }
         }
         return map
     }
 
     data class Board(val map: MutableMap<Int, MutableMap<Int, List<Point>>>, val time: Int) {
 
-        val maxX = map.keys.max() ?: 0
-        val minX = map.keys.min() ?: 0
-        val maxY = map.values.flatMap { it.keys }.max() ?: 0
-        val minY = map.values.flatMap { it.keys }.min() ?: 0
+        val maxY = map.keys.max() ?: 0
+        val minY = map.keys.min() ?: 0
+        val maxX = map.values.flatMap { it.keys }.max() ?: 0
+        val minX = map.values.flatMap { it.keys }.min() ?: 0
 
         val width = maxX - minX
         val height = maxY - minY
@@ -44,14 +44,11 @@ object Day10 {
     fun print(board: Board): String {
         val builder = StringBuilder()
 
-        val min = Math.min(board.minX, board.minY)
-        val max = Math.max(board.maxX, board.maxY)
+        for (y in (board.minY - 1)..(board.maxY + 1)) {
+            val row = board.map[y]
 
-        for (x in min..board.maxX) {
-            val row = board.map[x]
-
-            for (y in (board.maxY + 1) downTo  (min -1)) {
-                if (row?.get(y)?.isNotEmpty() == true) {
+            for (x in (board.minX - 1)..(board.maxX + 1)) {
+                if (row?.get(x)?.isNotEmpty() == true) {
                     builder.append("#")
                 } else {
                     builder.append(".")
@@ -75,11 +72,9 @@ object Day10 {
         val points = lines.map { parse(it) }
         var board = Board(pointsToMap(points), 0)
 
-        var maxBoard = board
-        var width = Int.MAX_VALUE
         var height = Int.MAX_VALUE
         for (i in 1..iterations) {
-            if(board.height < height){
+            if (board.height < height) {
                 height = board.height
 //                println("height $height")
             }
