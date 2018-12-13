d80e220ef36150453469804a5106fa137bdf126f
Cesar TL <cesartronlozai@convendia.com>
2018-12-13 13:33:26 +0000

    going crazy

---
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2018/Day13.kt
@@ -0,0 +1,258 @@
+package com.ctl.aoc.kotlin.y2018
+
+import java.lang.IllegalArgumentException
+import java.lang.StringBuilder
+
+
+sealed class Direction {
+    override fun toString(): String = this.javaClass.simpleName
+}
+
+object Up : Direction()
+object Right : Direction()
+object Down : Direction()
+object Left : Direction()
+
+fun Direction.turnLeft(): Direction {
+    return when (this) {
+        is Up -> Left   //  '^' ->  '<'
+        is Left -> Down //  '<' -> 'v'
+        is Down -> Right//  'v' -> '>'
+        is Right -> Up  //  '>' -> '6'
+    }
+}
+
+fun Direction.turnRight(): Direction {
+    return when (this) {
+        is Up -> Right
+        is Right -> Down
+        is Down -> Left
+        is Left -> Up
+    }
+}
+
+fun Direction.print(): String {
+    return when (this) {
+        Up -> "^"
+        Right -> ">"
+        Down -> "v"
+        Left -> "<"
+    }
+}
+
+
+sealed class TrackElement {
+    override fun toString(): String = this.javaClass.simpleName
+}
+
+object Horizontal : TrackElement()
+object Vertical : TrackElement()
+object UpRightLeftDownTurn : TrackElement()
+object UpLeftRightDownTurn : TrackElement()
+object Intersection : TrackElement()
+
+fun TrackElement.print(): String {
+    return when (this) {
+        Horizontal -> "-"
+        Vertical -> "|"
+        UpRightLeftDownTurn -> "/"
+        UpLeftRightDownTurn -> "\\"
+        Intersection -> "+"
+    }
+}
+
+object Day13 {
+
+    val intersectionDirections = listOf(Left, null, Right)
+
+    data class Position(val x: Int, val y: Int)
+
+    data class Cart(val id: Int, val direction: Direction, val nIntersection: Int = 0)
+
+    data class Track(val trackMap: Map<Int, Map<Int, TrackElement>>) {
+        fun trackElement(x: Int, y: Int): TrackElement? = trackMap[y]?.get(x)
+    }
+
+    fun parse(lines: List<String>): State {
+        val trackMap = mutableMapOf<Int, MutableMap<Int, TrackElement>>()
+        val cartMap = mutableMapOf<Int, MutableMap<Int, Cart>>()
+        val size = 9999
+
+        for (y in lines.indices) {
+            val line = lines[y]
+            val row = trackMap.computeIfAbsent(y) { mutableMapOf() }
+            for (x in line.indices) {
+                val id = y * size + x
+                val char = line[x]
+                val trackElement = when (char) {
+                    '-', '<', '>' -> Horizontal
+                    '|', '^', 'v' -> Vertical
+                    '\\' -> UpLeftRightDownTurn
+                    '/' -> UpRightLeftDownTurn
+                    '+' -> Intersection
+                    ' ' -> null
+                    else -> throw IllegalArgumentException("$char")
+                }
+                if (trackElement != null) {
+                    row[x] = trackElement
+                }
+                val cart = when (char) {
+                    '>' -> Cart(id, Right)
+                    '<' -> Cart(id, Left)
+                    '^' -> Cart(id, Up)
+                    'v' -> Cart(id, Down)
+                    else -> null
+                }
+                if (cart != null) {
+                    cartMap.computeIfAbsent(y) { mutableMapOf() }[x] = cart
+                }
+            }
+        }
+        val track = Track(trackMap)
+        return State(track, cartMap, 0)
+    }
+
+    fun Direction.move(p: Position): Position {
+        return when (this) {
+            Up -> Position(p.x, p.y - 1)
+            Right -> Position(p.x + 1, p.y)
+            Down -> Position(p.x, p.y + 1)
+            Left -> Position(p.x - 1, p.y)
+        }
+    }
+
+    fun TrackElement.rotateChart(cart: Cart): Cart {
+        return when (this) {
+            is Horizontal, Vertical -> cart
+            is UpRightLeftDownTurn -> { // '/'
+                val direction = when (cart.direction) {
+                    is Up, Down -> cart.direction.turnRight()
+                    is Left, Right -> cart.direction.turnLeft()
+                }
+                return cart.copy(direction = direction)
+            }
+            is UpLeftRightDownTurn -> { // '\'
+                val direction = when (cart.direction) {
+                    is Up, Down -> cart.direction.turnLeft()
+                    is Left, Right -> cart.direction.turnRight()
+                }
+                return cart.copy(direction = direction)
+            }
+            is Intersection -> {
+                val choice = intersectionDirections[cart.nIntersection % intersectionDirections.size]
+                val direction = when (choice) {
+                    is Left -> cart.direction.turnLeft()
+                    is Right -> cart.direction.turnRight()
+                    else -> cart.direction
+                }
+                return cart.copy(direction = direction, nIntersection = cart.nIntersection + 1)
+            }
+        }
+    }
+
+    data class State(val track: Track, val carts: MutableMap<Int, MutableMap<Int, Cart>>, val tick: Int) {
+        fun nextTick(destroyCart: Boolean = false): State {
+//            println("next tick")
+            val crashed = mutableSetOf<Int>()
+            val newMap = mutableMapOf<Int, MutableMap<Int, Cart>>()
+            var size = this.getCarts().size
+            carts.entries.sortedBy { it.key }.forEach { (y, row) ->
+                //                println("starting row $y")
+                row.entries.sortedBy { it.key }.forEach { (x, cart) ->
+                    //                    println("x: $x y: $y")
+                    if (!crashed.contains(cart.id)) {
+                        val newPosition = cart.direction.move(Position(x, y))
+                        val trackElement = track.trackElement(newPosition.x, newPosition.y)
+                                ?: throw IllegalArgumentException("no track at $newPosition")
+                        val newCart = trackElement.rotateChart(cart)
+
+
+                        val existingCart = carts[newPosition.y]?.get(newPosition.x)
+
+                        val crash1 = existingCart?.let { !crashed.contains(it.id)} ?: false
+                        val crash2 = newMap[newPosition.y]?.get(newPosition.x) != null
+
+                        if (crash1 || crash2) {
+                            if (!destroyCart) {
+                                throw IllegalArgumentException("Collision $newPosition at $tick")
+                            }
+                            if (crash1) {
+                                println("crash1")
+                                crashed.add(existingCart!!.id)
+                                crashed.add(cart.id)
+                            }
+                            else if (crash2) {
+                                println("crash2")
+//                                crashed.add(newPosition)
+                                newMap[newPosition.y]?.remove(newPosition.x)
+                            }
+                        } else {
+                            newMap.computeIfAbsent(newPosition.y) { mutableMapOf() }[newPosition.x] = newCart
+                        }
+                    }
+                }
+            }
+//            toBeRemoved.forEach { newMap[it.y]?.remove(it.x) }
+
+            return State(track, newMap, tick + 1)
+        }
+
+        fun getCarts(): List<Pair<Position, Cart>> = carts.entries.flatMap { (y, row) -> row.entries.map { Position(it.key, y) to it.value } }
+
+        fun print(): String {
+            val builder = StringBuilder()
+            val yMax = track.trackMap.keys.max() ?: 0
+            for (y in 0..yMax) {
+                val row = track.trackMap[y]!!
+                for (x in 0..(row.keys.max() ?: 0)) {
+                    builder.append(carts[y]?.get(x)?.direction?.print() ?: track.trackElement(x, y)?.print() ?: " ")
+                }
+                builder.append("\n")
+            }
+            return builder.toString()
+        }
+    }
+
+
+    fun solve1(lines: List<String>): State {
+        var state = parse(lines)
+        for (i in 0 until 150000) {
+            println(state.print())
+            println("**")
+            state = state.nextTick()
+//            println(state.tick)
+        }
+        return state
+    }
+
+    fun debug(lines: List<String>, n: Int, destroy: Boolean): State {
+        var state = parse(lines)
+        for (i in 0 until n) {
+            println(state.print())
+            state = state.nextTick(destroy)
+//            println(state.tick)
+        }
+        return state
+    }
+
+    fun solve2(lines: List<String>): Position {
+        var state = parse(lines)
+        var size = 999
+        var newSize: Int
+        while (state.getCarts().size > 1) {
+//            println(state.getCarts().size)
+//            println(state.print())
+            newSize = state.getCarts().size
+            if(newSize != size){
+                println("size $newSize")
+                size = newSize
+            }
+//            println("size ${state.getCarts().size}")
+            state = state.nextTick(true)
+        }
+//        println(state.print())
+        return state.getCarts().first().first
+    }
+
+
+}
---
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2018/Day13Test.kt
@@ -0,0 +1,30 @@
+package com.ctl.aoc.kotlin.y2018
+
+import com.ctl.aoc.kotlin.utils.InputUtils
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day13Test{
+
+    val input = InputUtils.getLines("2018", "day13.txt").toList()
+    val example = InputUtils.getLines("2018", "day13Example.txt").toList()
+    val jaques = InputUtils.getLines("2018", "day_13_Jaques.txt").toList()
+    val example2 = InputUtils.getLines("2018", "day13Example2.txt").toList()
+
+    val crash = InputUtils.getLines("2018", "Day13_crash.txt").toList()
+
+    @Test
+    internal fun solve1() {
+        var s = Day13.solve1(example)
+        println()
+//        assertEquals("", Day13.solve1(input))
+    }
+
+    @Test
+    internal fun solve2() {
+        assertEquals(Day13.Position(6, 4),Day13.solve2(example2))
+//        Day13.debug(crash, 20, true)
+        println(Day13.solve2(input)) //not 128,20, not 64,76, not 111/139
+//        println(Day13.solve2(jaques)) //not 128,20
+    }
+}
