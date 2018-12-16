008e95866c97cdf85d587e8e835642f0da8985f6
Cesar TL <cesartronlozai@convendia.com>
2018-12-16 09:35:17 +0000

    Day 16 part 1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2018/Day16.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2018/Day16.kt
@@ -1,7 +1,9 @@
 package com.ctl.aoc.kotlin.y2018
 
+import com.ctl.aoc.kotlin.y2018.Day16.Opcode.*
 import com.ctl.aoc.kotlin.y2018.Day16.Value.Immediate
 import com.ctl.aoc.kotlin.y2018.Day16.Value.Register
+import java.util.regex.Pattern
 
 object Day16 {
 
@@ -12,11 +14,13 @@ object Day16 {
     sealed class Value {
         data class Immediate(val value: Int) : Value()
         data class Register(val register: Int) : Value()
+        object Nil : Value()
 
         fun getValue(state: State): Int {
             return when (this) {
                 is Immediate -> this.value
                 is Register -> state.registers[this.register] ?: 0
+                is Nil -> -1
             }
         }
     }
@@ -91,7 +95,7 @@ object Day16 {
 
         // assignment
         abstract class Set : Opcode() {
-            override fun bValue(b: Int): Value = TODO("")
+            override fun bValue(b: Int): Value = Value.Nil
             override fun cValue(aV: Int, bV: Int): Int = aV
         }
 
@@ -105,7 +109,7 @@ object Day16 {
 
         // gt
         abstract class Gt : Opcode() {
-            override fun cValue(aV: Int, bV: Int): Int = if(aV > bV) 1 else 0
+            override fun cValue(aV: Int, bV: Int): Int = if (aV > bV) 1 else 0
         }
 
         object gtir : Gt() {
@@ -125,7 +129,7 @@ object Day16 {
 
         // eq
         abstract class Eq : Opcode() {
-            override fun cValue(aV: Int, bV: Int): Int = if(aV == bV) 1 else 0
+            override fun cValue(aV: Int, bV: Int): Int = if (aV == bV) 1 else 0
         }
 
         object eqir : Eq() {
@@ -144,5 +148,45 @@ object Day16 {
         }
     }
 
+    val opcodes = listOf(addr, addi, mulr, muli, banr, bani, borr, bori, gtir, gtri, gtrr, eqir, eqri, eqrr, setr, seti)
+
+    data class TestSpec(val opCode: Int, val a: Int, val b: Int, val c: Int, val before: State, val after: State)
+
+    val beforePattern = Pattern.compile("Before: \\[(.+)\\]")
+    val afterPattern = Pattern.compile("After: \\[(.+)\\]")
+
+    fun parseState(s: String): State {
+        val registers = s.split(",").map { it.trim() }.mapIndexed { index, s -> index to s.toInt() }.toMap()
+        return State(registers)
+    }
+
+    fun parseSpec(lines: List<String>): TestSpec {
+        val before = parseState(lines[0].substring(9).dropLast(1))
+        val after = parseState(lines[2].substring(9).dropLast(1))
+        val op = lines[1].split(" ").map { it.toInt() }
+        return TestSpec(opCode = op[0], a = op[1], b = op[2], c = op[3], before = before, after = after)
+    }
+
+    fun matchSpec(opcode: Opcode, spec: TestSpec): Boolean {
+        val exec = opcode.exec(spec.a, spec.b, spec.c)
+        return exec(spec.before) == spec.after
+    }
+
+    fun matchForSpec(spec: TestSpec): List<Opcode> = opcodes.filter { matchSpec(it, spec) }
+
+    fun allSpecs(lines: List<String>): List<TestSpec> {
+        val l = mutableListOf<TestSpec>()
+        var i = 0
+        while (i < lines.size) {
+            l.add(parseSpec(lines.subList(i, i + 3)))
+            i += 4
+        }
+        return l.toList()
+    }
+
+    fun solve1(input: List<String>): Int {
+        val specs = allSpecs(input)
+        return specs.map { matchForSpec(it).size }.count { it >= 3 }
+    }
 
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2018/Day16Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2018/Day16Test.kt
@@ -1,3 +1,26 @@
 package com.ctl.aoc.kotlin.y2018
 
-internal class Day16Test
+import com.ctl.aoc.kotlin.utils.InputUtils
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day16Test{
+    val input = InputUtils.getLines("2018", "day16_1.txt").toList()
+    val spec1 = """Before: [3, 2, 1, 1]
+9 2 1 2
+After:  [3, 2, 2, 1]
+"""
+
+    @Test
+    internal fun parseSpec() {
+        val spec = Day16.parseSpec(spec1.split("\n").map { it.trim() })
+        println(spec)
+
+        assertEquals(3, Day16.matchForSpec(spec).size)
+    }
+
+    @Test
+    internal fun testPart1() {
+        println(Day16.solve1(input))
+    }
+}
