06f05bd7f593914c34c8a13214f0d87cf4792592
Cesar TL <cesar.tronlozai@gmai.com>
2019-12-08 14:10:27 +0000

    2019 Day6 revisited

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2019/Day6.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2019/Day6.kt
@@ -4,6 +4,7 @@ import com.ctl.aoc.kotlin.utils.Dijkstra
 import com.ctl.aoc.kotlin.utils.Graph
 import com.ctl.aoc.kotlin.utils.dijkstra
 import com.ctl.aoc.kotlin.utils.findPath
+import java.util.*
 
 object Day6 {
 
@@ -18,6 +19,63 @@ object Day6 {
         return steps.values.sum()
     }
 
+    data class Node(val name: String, val parent: Node? = null)
+
+    tailrec fun Node.pathToRoot(acc: MutableList<Node> = mutableListOf()): List<Node> {
+        acc.add(this)
+        if (this.parent == null) {
+            return acc
+        }
+        return parent.pathToRoot(acc)
+    }
+
+    fun solve1Bis(lines: Sequence<String>): Int {
+        val nodeMap = computeNodeMap(lines)
+        val distances = mutableMapOf<String, Int>("COM" to 0)
+        nodeMap.values.forEach { computeDistances(distances, it) }
+        return distances.values.sum()
+    }
+
+    private fun computeNodeMap(lines: Sequence<String>): MutableMap<String, Node> {
+        val queue: Deque<Pair<String, String>> = ArrayDeque()
+        lines.map { it.split(")") }.map { it[0] to it[1] }.forEach { queue.add(it) }
+        val nodeMap = mutableMapOf<String, Node>()
+        nodeMap["COM"] = Node("COM")
+
+        while (queue.isNotEmpty()) {
+            val (to, from) = queue.removeFirst()
+            val parent = nodeMap[to]
+            if (parent != null) {
+                nodeMap[from] = Node(from, parent)
+            } else {
+                queue.addLast((from to to))
+            }
+        }
+        return nodeMap
+    }
+
+    fun solve2Bis(lines: Sequence<String>): Int {
+        val nodeMap = computeNodeMap(lines)
+        val path1 = nodeMap["YOU"]!!.pathToRoot().reversed()
+        val path2 = nodeMap["SAN"]!!.pathToRoot().reversed()
+
+        var i = 0
+        while (path1[i] == path2[i]) {
+            i++
+        }
+        return (path1.size -1) + (path2.size - 1) - 2 * (i - 1) - 2
+    }
+
+    private fun computeDistances(distances: MutableMap<String, Int>, node: Node) {
+        if (node.parent != null) {
+            if (!distances.containsKey(node.parent.name)) {
+                computeDistances(distances, node.parent)
+            }
+            val parent = distances[node.parent.name]!!
+            distances[node.name] = parent + 1
+        }
+    }
+
     fun solve2(lines: Sequence<String>): Int {
         val graph = Graph<String>()
         lines.map { it.split(")") }.map { it[0] to it[1] }.forEach { (from, to) ->
@@ -25,7 +83,7 @@ object Day6 {
         }
         val (steps, previous) = graph.dijkstra("YOU", "SAN")
 
-        return findPath("SAN", previous).size -3
+        return findPath("SAN", previous).size - 3
     }
 
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2019/Day6Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2019/Day6Test.kt
@@ -37,13 +37,24 @@ I)SAN""".split("\n").asSequence()
     @Test
     fun solve1() {
         assertThat(Day6.solve1(example1)).isEqualTo(42)
+        assertThat(Day6.solve1Bis(example1)).isEqualTo(42)
         println(Day6.solve1(puzzleInput))
+        assertThat(Day6.solve1Bis(puzzleInput)).isEqualTo(106065)
     }
 
 
     @Test
     internal fun solve2() {
         println(Day6.solve2(example2))
-        println(Day6.solve2(puzzleInput))
+        assertThat(Day6.solve2Bis(example2)).isEqualTo(4)
+
+        val t1 = System.currentTimeMillis()
+        assertThat(Day6.solve2(puzzleInput)).isEqualTo(253)
+        val t2 = System.currentTimeMillis()
+        assertThat(Day6.solve2Bis(puzzleInput)).isEqualTo(253)
+        val t3 = System.currentTimeMillis()
+
+        println("Solution 1: ${t2 - t1}ms")
+        println("Solution 1bis: ${t3 - t2}ms")
     }
 }
