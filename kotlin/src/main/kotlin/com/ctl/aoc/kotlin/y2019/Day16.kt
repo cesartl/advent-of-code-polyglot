d3eeaa3be7e66f9e10e21dfc965c8fa0db51c895
Cesar TL <cesar.tronlozai@gmai.com>
2019-12-16 07:25:21 +0000

    2019 Day16 WIP

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2019/Day16.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2019/Day16.kt
@@ -4,12 +4,13 @@ object Day16 {
 
     val pattern = sequenceOf(0, 1, 0, -1)
 
-    fun solve1(numbers: Sequence<Int>): Int {
-
-        TODO()
+    fun solve1(signal: String): String {
+        return applyFFT(100, signal).take(8)
     }
 
-    fun patterForIndex(idx: Int): Sequence<Int> {
+    private val patternCache = mutableMapOf<Int, IntArray>()
+
+    private fun patterForIndex(idx: Int): Sequence<Int> {
         return sequence {
             while (true) {
                 pattern.forEach { value ->
@@ -21,19 +22,32 @@ object Day16 {
         }.drop(1)
     }
 
-    fun processOneDigit(idx: Int, signal: Sequence<Int>): Int {
-        return signal.zip(patterForIndex(idx)) { x, y ->
+    private fun cachedPatternForIndex(size: Int, idx: Int): IntArray {
+        return patternCache.computeIfAbsent(idx) { arrayPattern(size, it) }
+    }
+
+    fun arrayPattern(size: Int, idx: Int): IntArray {
+        val pattern = patterForIndex(idx)
+        val iterator = pattern.iterator()
+        return IntArray(size) {
+            iterator.next()
+        }
+    }
+
+    private fun processOneDigit(idx: Int, signal: IntArray): Int {
+        return signal.zip(cachedPatternForIndex(size = signal.size, idx = idx)) { x, y ->
             x * y
         }.sum().toString().last().toInt() - '0'.toInt()
     }
 
-    fun applyFFTOnce(signal: Sequence<Int>): Sequence<Int> {
-        return signal.mapIndexed { idx, _ ->
-            processOneDigit(idx, signal)
+    private fun applyFFTOnce(signal: IntArray): IntArray {
+        signal.indices.forEach { idx ->
+            signal[idx] = processOneDigit(idx, signal)
         }
+        return signal
     }
 
-    tailrec fun applyFFT(n: Int, signal: Sequence<Int>): Sequence<Int> {
+    private tailrec fun applyFFT(n: Int, signal: IntArray): IntArray {
         return if (n == 0) {
             signal
         } else {
@@ -41,7 +55,7 @@ object Day16 {
         }
     }
 
-    fun parseSignal(s: String): Sequence<Int> = s.map { it.toInt() - '0'.toInt() }.asSequence()
+    private fun parseSignal(s: String): IntArray = s.map { it.toInt() - '0'.toInt() }.toIntArray()
 
     fun applyFFT(n: Int, signal: String): String {
         return applyFFT(n, parseSignal(signal)).joinToString("")
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2019/Day16Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2019/Day16Test.kt
@@ -10,14 +10,19 @@ internal class Day16Test {
 
     @Test
     fun solve1() {
-        println(Day16.applyFFT(2, puzzleInput))
+        assertThat(Day16.solve1("80871224585914546619083218645595")).isEqualTo("24176176")
+        assertThat(Day16.solve1("19617804207202209144916044189917")).isEqualTo("73745418")
+        assertThat(Day16.solve1("69317163492948606335995924319873")).isEqualTo("52432133")
+        println(Day16.solve1(puzzleInput)) //> 80424724
+    }
+
+    @Test
+    internal fun array() {
+        println(Day16.arrayPattern(20, 2).toList())
     }
 
     @Test
     internal fun fft() {
-        assertThat(Day16.applyFFTOnce(sequenceOf(1, 2, 3, 4, 5, 6, 7, 8)).toList())
-                .isEqualTo(listOf(4, 8, 2, 2, 6, 1, 5, 8))
-
         assertThat(Day16.applyFFT(1, "12345678")).isEqualTo("48226158")
         assertThat(Day16.applyFFT(2, "12345678")).isEqualTo("34040438")
         assertThat(Day16.applyFFT(3, "12345678")).isEqualTo("03415518")
