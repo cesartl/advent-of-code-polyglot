46872589593abaebe9770c0e9fabb1092ef24de3
Cesar TL <cesar.tronlozai@gmai.com>
2019-12-16 18:06:06 +0000

    2019 Day16 finished

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2019/Day16.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2019/Day16.kt
@@ -1,14 +1,38 @@
 package com.ctl.aoc.kotlin.y2019
 
+import kotlin.math.abs
+
 object Day16 {
 
     val pattern = sequenceOf(0, 1, 0, -1)
 
     fun solve1(signal: String): String {
-        return applyFFT(100, signal).take(8)
+        println("length ${signal.length}")
+        return applyFFT(100, signal).joinToString("").take(8)
     }
 
-    private val patternCache = mutableMapOf<Int, LongArray>()
+    fun solve2(signal: String): String {
+        val skip = signal.take(7).toInt()
+        println("Offset: $skip")
+        val bigSignal = multiplySignal(10000, signal)
+        println("length after multiplication ${bigSignal.length}")
+        val fft = applyFFTOptimised(100, parseSignal(bigSignal.drop(skip)))
+        return buildString {
+            (0 until 8).forEach { i ->
+                append(fft[i])
+            }
+        }
+    }
+
+    private fun multiplySignal(n: Int, signal: String): String {
+        return buildString {
+            (0 until n).forEach { _ ->
+                append(signal)
+            }
+        }
+    }
+
+    private val patternCache = mutableMapOf<String, LongArray>()
 
     private fun patterForIndex(idx: Int): Sequence<Int> {
         return sequence {
@@ -22,42 +46,67 @@ object Day16 {
         }.drop(1)
     }
 
-    private fun cachedPatternForIndex(size: Int, idx: Int): LongArray {
-        return patternCache.computeIfAbsent(idx) { arrayPattern(size, it) }
+    private fun cachedPatternForIndex(size: Int, idx: Int, offset: Int): LongArray {
+        return patternCache.computeIfAbsent("$size-$idx-$offset") { arrayPattern(size, idx, offset) }
     }
 
-    fun arrayPattern(size: Int, idx: Int): LongArray {
-        val pattern = patterForIndex(idx)
+    fun arrayPattern(size: Int, idx: Int, offset: Int): LongArray {
+        val pattern = patterForIndex(idx + offset).drop(offset)
         val iterator = pattern.iterator()
         return LongArray(size) {
             iterator.next().toLong()
         }
     }
 
-    private fun processOneDigit(idx: Int, signal: LongArray): Long {
-        return signal.zip(cachedPatternForIndex(size = signal.size, idx = idx)) { x, y ->
-            x * y
-        }.sum().toString().last().toLong() - '0'.toLong()
+    private fun processOneDigit(idx: Int, signal: LongArray, offset: Int): Long {
+        val pattern = cachedPatternForIndex(size = signal.size, idx = idx, offset = offset)
+        var count = 0L
+        (signal.indices).forEach { i ->
+            count += (signal[i] * pattern[i]) % 10
+        }
+        return abs(count) % 10L
     }
 
-    private fun applyFFTOnce(signal: LongArray): LongArray {
-        signal.indices.forEach { idx ->
-            signal[idx] = processOneDigit(idx, signal)
+    private fun processOneDigitFast(idx: Int, signal: LongArray, total: Long): Long {
+        TODO()
+    }
+
+    private fun applyFFTOnce(signal: LongArray, offset: Int): LongArray {
+        (signal.indices).forEach { idx ->
+            signal[idx] = processOneDigit(idx, signal, offset)
         }
         return signal
     }
 
-    private tailrec fun applyFFT(n: Int, signal: LongArray): LongArray {
+    private fun applyFFTOptimisedOnce(signal: LongArray): LongArray {
+        var sumSoFar = 0L
+        (signal.indices).reversed().forEach { idx ->
+            sumSoFar += signal[idx]
+            signal[idx] = abs(sumSoFar) % 10L
+        }
+        return signal
+    }
+
+    private tailrec fun applyFFT(n: Int, signal: LongArray, offset: Int): LongArray {
         return if (n == 0) {
             signal
         } else {
-            applyFFT(n - 1, applyFFTOnce(signal))
+            applyFFT(n - 1, applyFFTOnce(signal, offset), offset)
+        }
+    }
+
+    private tailrec fun applyFFTOptimised(n: Int, signal: LongArray): LongArray {
+        return if (n == 0) {
+            signal
+        } else {
+            applyFFTOptimised(n - 1, applyFFTOptimisedOnce(signal))
         }
     }
 
     private fun parseSignal(s: String): LongArray = s.map { it.toLong() - '0'.toLong() }.toLongArray()
 
-    fun applyFFT(n: Int, signal: String): String {
-        return applyFFT(n, parseSignal(signal)).joinToString("")
+    fun applyFFT(n: Int, signal: String, offset: Int = 0): LongArray {
+        println("Signal length ${signal.length}")
+        return applyFFT(n, parseSignal(signal), offset)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2019/Day16Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2019/Day16Test.kt
@@ -13,19 +13,22 @@ internal class Day16Test {
         assertThat(Day16.solve1("80871224585914546619083218645595")).isEqualTo("24176176")
         assertThat(Day16.solve1("19617804207202209144916044189917")).isEqualTo("73745418")
         assertThat(Day16.solve1("69317163492948606335995924319873")).isEqualTo("52432133")
-        println(Day16.solve1(puzzleInput)) //> 80424724
+        assertThat(Day16.solve1(puzzleInput)).isEqualTo("88323090")
     }
 
     @Test
-    internal fun array() {
-        println(Day16.arrayPattern(20, 2).toList())
+    internal fun solve2() {
+        assertThat(Day16.solve2("03036732577212944063491565474664")).isEqualTo("84462026")
+        assertThat(Day16.solve2("02935109699940807407585447034323")).isEqualTo("78725270")
+        assertThat(Day16.solve2("03081770884921959731165446850517")).isEqualTo("53553731")
+        println(Day16.solve2(puzzleInput))
     }
 
     @Test
     internal fun fft() {
-        assertThat(Day16.applyFFT(1, "12345678")).isEqualTo("48226158")
-        assertThat(Day16.applyFFT(2, "12345678")).isEqualTo("34040438")
-        assertThat(Day16.applyFFT(3, "12345678")).isEqualTo("03415518")
-        assertThat(Day16.applyFFT(4, "12345678")).isEqualTo("01029498")
+        assertThat(Day16.applyFFT(1, "12345678").joinToString("")).isEqualTo("48226158")
+        assertThat(Day16.applyFFT(2, "12345678").joinToString("")).isEqualTo("34040438")
+        assertThat(Day16.applyFFT(3, "12345678").joinToString("")).isEqualTo("03415518")
+        assertThat(Day16.applyFFT(4, "12345678").joinToString("")).isEqualTo("01029498")
     }
 }
