a754c5b343e62a2667c84e748cf3d9d439b4cad7
Cesar TL <cesar.tronlozai@gmai.com>
2019-12-27 11:01:32 +0100

    2019 Day22 WIP

---
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2019/Day22.kt
@@ -0,0 +1,88 @@
+package com.ctl.aoc.kotlin.y2019
+
+import java.util.*
+
+typealias Deck = Sequence<Int>
+
+object Day22 {
+
+    sealed class ShuffleTechnique {
+        abstract fun shuffle(deck: Deck): Deck
+
+        object DealNew : ShuffleTechnique() {
+            override fun shuffle(deck: Deck): Deck {
+                return sequence {
+                    val stack: Deque<Int> = ArrayDeque()
+                    deck.forEach { stack.addFirst(it) }
+                    while (stack.isNotEmpty()) {
+                        yield(stack.removeFirst())
+                    }
+                }
+            }
+        }
+
+        data class Cut(val n: Int) : ShuffleTechnique() {
+            override fun shuffle(deck: Deck): Deck {
+                val cut = deck.take(n)
+                return sequence {
+                    yieldAll(deck.drop(n))
+                    yieldAll(cut)
+                }
+            }
+        }
+
+        data class Deal(val n: Int) : ShuffleTechnique() {
+            override fun shuffle(deck: Deck): Deck {
+                val old = deck.toList()
+                return sequence {
+                    old.indices.forEach { i ->
+                        yield(old[i * (old.size - n) % old.size])
+                    }
+                }
+            }
+        }
+
+        companion object {
+            private val dealRegex = """deal with increment ([\d]+)""".toRegex()
+            private val cutRegex = """cut ([-\d]+)""".toRegex()
+
+            fun parse(s: String, size: Int): ShuffleTechnique {
+                val dealMatch = dealRegex.matchEntire(s)
+                val cutMatch = cutRegex.matchEntire(s)
+                return if (dealMatch != null) {
+                    val n = dealMatch.groupValues[1].toInt()
+                    Deal(n)
+                } else if (cutMatch != null) {
+                    val n = cutMatch.groupValues[1].toInt()
+                    if (n > 0) {
+                        Cut(n)
+                    } else {
+                        Cut(size + n)
+                    }
+                } else if (s == "deal into new stack") {
+                    DealNew
+                } else {
+                    throw IllegalArgumentException(s)
+                }
+            }
+        }
+    }
+
+    fun parse(lines: Sequence<String>, size: Int): Sequence<ShuffleTechnique> {
+        return lines.map { ShuffleTechnique.parse(it, size) }
+    }
+
+    fun apply(deck: Deck, techniques: Sequence<ShuffleTechnique>): Deck {
+        return techniques.fold(deck) { d, t ->
+            t.shuffle(d)
+        }
+    }
+
+    fun solve1(lines: Sequence<String>): Int {
+        val end = 10006
+        val techniques = parse(lines, end + 1)
+        val deck = (0..end).asSequence()
+        val after = apply(deck, techniques)
+        return after.indexOf(2019)
+    }
+}
---
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2019/Day22Test.kt
@@ -0,0 +1,89 @@
+package com.ctl.aoc.kotlin.y2019
+
+import com.ctl.aoc.kotlin.utils.InputUtils
+import com.ctl.aoc.kotlin.y2019.Day22.ShuffleTechnique.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Day22Test {
+
+    val puzzleInput = InputUtils.getLines(2019, 22)
+
+    val example1 = """deal with increment 7
+deal into new stack
+deal into new stack""".splitToSequence('\n')
+
+    val example2 = """cut 6
+deal with increment 7
+deal into new stack""".splitToSequence('\n')
+
+    val example3 = """deal with increment 7
+deal with increment 9
+cut -2""".splitToSequence('\n')
+
+    val example4 = """deal into new stack
+cut -2
+deal with increment 7
+cut 8
+cut -4
+deal with increment 7
+cut 3
+deal with increment 9
+deal with increment 3
+cut -1""".splitToSequence('\n')
+
+    @Test
+    internal fun dealNewTest() {
+        val deck = sequenceOf(0, 1, 2, 3, 4)
+        assertThat(DealNew.shuffle(deck).toList())
+                .containsExactlyElementsOf(listOf(4, 3, 2, 1, 0))
+    }
+
+    @Test
+    internal fun cutTest() {
+        val deck = sequenceOf(0, 1, 2, 3, 4)
+        assertThat(Cut(3).shuffle(deck).toList())
+                .containsExactlyElementsOf(listOf(3, 4, 0, 1, 2))
+
+        val t = Day22.ShuffleTechnique.parse("cut -4", 10)
+        val deck10 = (0..9).asSequence()
+        assertThat(t.shuffle(deck10).toList())
+                .isEqualTo("6 7 8 9 0 1 2 3 4 5".split(" ").map { it.toInt() }.toList())
+    }
+
+    @Test
+    internal fun testDeal() {
+        val deck = (0..9).asSequence()
+        assertThat(Deal(3).shuffle(deck).toList())
+                .containsExactlyElementsOf(listOf(0, 7, 4, 1, 8, 5, 2, 9, 6, 3))
+
+
+    }
+
+    @Test
+    internal fun testLogic() {
+        val deck = (0..9).asSequence()
+        val e1 = Day22.apply(deck, Day22.parse(example1, 10)).toList()
+        assertThat(e1).isEqualTo(listOf(0, 3, 6, 9, 2, 5, 8, 1, 4, 7))
+
+        val e2 = Day22.apply(deck, Day22.parse(example2, 10)).toList()
+        assertThat(e2).isEqualTo("3 0 7 4 1 8 5 2 9 6".split(" ").map { it.toInt() }.toList())
+
+        val e3 = Day22.apply(deck, Day22.parse(example3, 10)).toList()
+        assertThat(e3).isEqualTo("6 3 0 7 4 1 8 5 2 9".split(" ").map { it.toInt() }.toList())
+
+        val e4 = Day22.apply(deck, Day22.parse(example4, 10)).toList()
+        assertThat(e4)
+                .isEqualTo(listOf(9, 2, 5, 8, 1, 4, 7, 0, 3, 6))
+    }
+
+    @Test
+    fun solve1() {
+        val deck = (0..9).asSequence()
+        val toList = Day22.apply(deck, Day22.parse(example4, 10)).toList()
+        println(toList)
+        assertThat(toList)
+                .containsExactly(9, 2, 5, 8, 1, 4, 7, 0, 3, 6)
+        println(Day22.solve1(puzzleInput)) // > 1156
+    }
+}
