b9efecc41d1e497072ec4b072003aec221b412b9
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-04 08:00:05 +0000

    2020 Day 4

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day4.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day4.kt
@@ -2,10 +2,80 @@ package com.ctl.aoc.kotlin.y2020
 
 object Day4 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val fields = parseFields(input)
+
+        return fields.filter {
+            it.containsKey("byr")
+                    && it.containsKey("iyr")
+                    && it.containsKey("eyr")
+                    && it.containsKey("hgt")
+                    && it.containsKey("hcl")
+                    && it.containsKey("ecl")
+                    && it.containsKey("pid")
+        }.count()
     }
 
+    val hgtRegex = """([0-9]+)(cm|in)""".toRegex()
+    val hairColourRegex = """#[0-9a-f]{6}""".toRegex()
+
+    val validators = mapOf<String, (String) -> Boolean>(
+            "byr" to { s -> s.all { c -> c.isDigit() } && s.toInt() in (1920..2002) },
+            "iyr" to { s -> s.all { c -> c.isDigit() } && s.toInt() in (2010..2020) },
+            "eyr" to { s -> s.all { c -> c.isDigit() } && s.toInt() in (2020..2030) },
+            "hgt" to { s ->
+                hgtRegex.matchEntire(s)?.let { match ->
+                    val height = match.groupValues[1].toInt()
+                    if (match.groupValues[2] == "cm") {
+                        height in (150..193)
+                    } else {
+                        height in (59..76)
+                    }
+
+                } ?: false
+            },
+            "hcl" to { s -> hairColourRegex.matchEntire(s) != null },
+            "ecl" to { s -> setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(s) },
+            "pid" to { s -> s.all { c -> c.isDigit() } && s.length == 9 }
+    )
+
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        val fields = parseFields(input)
+        return fields.filter {
+            it.containsKey("byr")
+                    && it.containsKey("iyr")
+                    && it.containsKey("eyr")
+                    && it.containsKey("hgt")
+                    && it.containsKey("hcl")
+                    && it.containsKey("ecl")
+                    && it.containsKey("pid")
+                    && it.all { entry ->
+                validate(entry.key, entry.value)
+            }
+        }.count()
+    }
+
+    fun validate(field: String, value: String): Boolean {
+        return validators[field]?.let { validator ->
+            validator(value)
+        } ?: true
+    }
+
+    private fun parseFields(input: Sequence<String>): List<Map<String, String>> {
+        val (full, last) = input.fold(listOf<String>() to "") { (acc, current), line ->
+            if (line.isEmpty()) {
+                (acc + current) to ""
+            } else {
+                acc to ("$current$line ")
+            }
+        }
+        val entries = full + last
+
+        return entries.map { entry ->
+            entry.trim().split(" ")
+                    .map { field ->
+                        val s = field.split(":")
+                        s[0] to s[1]
+                    }.toMap()
+        }
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day4Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day4Test.kt
@@ -7,13 +7,75 @@ internal class Day4Test {
 
     val puzzleInput = InputUtils.getLines(2020, 4)
 
+    val example = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in""".split("\n").asSequence()
+
+    val allValid = """pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719""".splitToSequence("\n")
+
     @Test
     fun solve1() {
-        Day4.solve1(puzzleInput)
+        println(Day4.solve1(example))
+        println(Day4.solve1(puzzleInput))
+    }
+
+    @Test
+    internal fun validate() {
+        checkValid("byr", "2002")
+        checkInvalid("byr", "2003")
+
+        checkValid("hgt", "60in")
+        checkValid("hgt", "190cm")
+        checkInvalid("hgt", "190in")
+        checkInvalid("hgt", "190")
+
+        checkValid("hcl", "#123abc")
+        checkInvalid("hcl", "#123abz")
+        checkInvalid("hcl", "123abc")
+
+        checkValid("ecl", "amb")
+        checkInvalid("ecl", "aa")
+
+        checkValid("pid", "093154719")
+        checkInvalid("pid", "3556412378")
+
+        checkValid("cid", "093154719")
+
+        assert(Day4.solve2(allValid) == 4)
+    }
+
+    private fun checkValid(field: String, value: String) {
+        assert(Day4.validate(field, value))
+    }
+
+    private fun checkInvalid(field: String, value: String) {
+        assert(!Day4.validate(field, value))
     }
 
     @Test
     fun solve2() {
-        Day4.solve2(puzzleInput)
+        println(Day4.solve2(example))
+        println(Day4.solve2(puzzleInput))
     }
 }
