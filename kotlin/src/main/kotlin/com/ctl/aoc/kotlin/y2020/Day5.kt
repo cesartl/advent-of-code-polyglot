6e27897299e57c41e114ade79eef072aeff06ab7
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-05 19:34:02 +0000

    2020 Day 5

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day5.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day5.kt
@@ -1,11 +1,42 @@
 package com.ctl.aoc.kotlin.y2020
 
 object Day5 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    data class Seat(val row: Int, val col: Int) {
+
+        val id: Int = row * 8 + col
+
+        companion object {
+            fun parse(s: String): Seat {
+                val row = toInt(s.take(7), 'B')
+                val col = toInt(s.drop(7), 'R')
+                return Seat(row, col)
+            }
+        }
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: Sequence<String>): Int? {
+        return input.map { Seat.parse(it).id }.max()
+    }
+
+    fun solve2(input: Sequence<String>): Int? {
+        val seats = input.map { Seat.parse(it) }.sortedBy { it.id }.toList()
+        val ids = seats.map { it.id }.toSet()
+        val min = seats.minBy { it.id }!!
+        val max = seats.maxBy { it.id }!!
+        println("min $min ${min.id}")
+        println("max $max ${max.id}")
+        val start = Seat(min.row, 7).id + 1
+        val end = Seat(max.row, 0).id
+        (start until end).forEach {
+            if (!ids.contains(it)) {
+                return it
+            }
+        }
+        return null
+    }
+
+    fun toInt(s: String, upperChar: Char): Int {
+        return Integer.parseInt(s.map { if (it == upperChar) '1' else '0' }.joinToString(""), 2)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day5Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day5Test.kt
@@ -1,6 +1,7 @@
 package com.ctl.aoc.kotlin.y2020
 
 import com.ctl.aoc.kotlin.utils.InputUtils
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day5Test {
@@ -8,12 +9,23 @@ internal class Day5Test {
     val puzzleInput = InputUtils.getLines(2020, 5)
 
     @Test
+    fun toInt() {
+        println(Day5.toInt("FBFBBFF", 'B'))
+        println(Day5.toInt("RLR", 'R'))
+    }
+
+    @Test
+    fun seat() {
+        assertEquals(567, Day5.Seat.parse("BFFFBBFRRR").id)
+    }
+
+    @Test
     fun solve1() {
-        Day5.solve1(puzzleInput)
+        println(Day5.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        Day5.solve2(puzzleInput)
+        println(Day5.solve2(puzzleInput))
     }
 }
