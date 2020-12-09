8ec73b9f794de57cea8c71e1e63da5a2b29bd11b
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-09 07:47:26 +0000

    2020 Day 9

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day9.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day9.kt
@@ -1,11 +1,47 @@
 package com.ctl.aoc.kotlin.y2020
 
 object Day9 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: Sequence<String>, size: Int = 25): Long {
+        val numbers = input.map { it.toLong() }.toList()
+        val init = numbers.take(size)
+        numbers.drop(size).fold(init) { previous, n ->
+            if (isValid(previous, n)) {
+                previous.drop(1) + listOf(n)
+            } else {
+                return n
+            }
+        }
+        throw Error("Not found")
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun isValid(previous: List<Long>, n: Long): Boolean {
+        val set = mutableSetOf<Long>()
+        previous.forEach { i ->
+            val target = n - i
+            if (set.contains(target)) {
+                return true
+            }
+            set.add(i)
+        }
+        return false
+    }
+
+    fun solve2(input: Sequence<String>, size: Int = 25): Long {
+        val target = solve1(input, size)
+        val numbers = input.map { it.toLong() }.toList()
+
+        numbers.indices.forEach { start ->
+            val candidates = mutableListOf<Long>()
+            var i = start + 1
+            while (i < numbers.size && candidates.sum() <= target) {
+                candidates.add(numbers[i])
+                i++
+            }
+            candidates.removeAt(candidates.size - 1)
+            if (candidates.sum() == target) {
+                return (candidates.min() ?: 0L) + (candidates.max() ?: 0L)
+            }
+        }
+        throw Error("Not found")
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day9Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day9Test.kt
@@ -7,13 +7,37 @@ internal class Day9Test {
 
     val puzzleInput = InputUtils.getLines(2020, 9)
 
+    val example = """35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576""".splitToSequence("\n")
+
     @Test
     fun solve1() {
-        Day9.solve1(puzzleInput)
+//        println(Day9.solve1(example))
+        println(Day9.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        Day9.solve2(puzzleInput)
+        //1105310476
+        println(Day9.solve2(example, 5))
+        println(Day9.solve2(puzzleInput))
     }
 }
