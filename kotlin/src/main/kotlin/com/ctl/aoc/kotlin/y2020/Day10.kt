ea1e4d8896a6fa2ed3e26dce7def9cf0c9cdfe25
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-10 17:23:52 +0000

    2020 Day 10

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day10.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day10.kt
@@ -2,10 +2,58 @@ package com.ctl.aoc.kotlin.y2020
 
 object Day10 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val outlets = input.map { it.toInt() }.toSet()
+        val chain = find(0, listOf(), outlets, outlets.size)
+        println(listOf(0) + chain)
+
+        var (one, three) = count(chain)
+        return one * three
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun count(chain: List<Int>): Pair<Int, Int> {
+        var one = 0
+        var three = 0
+        (chain).zipWithNext().forEach { (n, next) ->
+            if (next - n == 3) {
+                three++
+            } else {
+                one++
+            }
+        }
+        three++
+        println(one)
+        println(three)
+        return Pair(one, three)
+    }
+
+    fun find(start: Int, path: List<Int>, remaining: Set<Int>, targetLength: Int): List<Int> {
+        if (remaining.isEmpty()) {
+            return path
+        }
+        return listOf(start + 1, start + 2, start + 3)
+                .asSequence()
+                .filter { remaining.contains(it) }
+                .map { newStart -> find(newStart, path + listOf(newStart), remaining - newStart, targetLength) }
+                .firstOrNull() { it.size == targetLength }
+                ?: listOf()
+    }
+
+    fun solve2(input: Sequence<String>): Long {
+        val outlets = input.map { it.toInt() }.sortedDescending().toList() + 0
+        println(outlets)
+        val dp = mutableMapOf<Int, Long>()
+        dp[outlets.first()] = 1
+
+        outlets.drop(1).forEach { jolt ->
+            println("doing $jolt dp=$dp")
+            var ways = 0L
+            (jolt + 1..jolt + 3).forEach { adapter ->
+                val way = dp[adapter] ?: 0
+                println("Checking ways for $adapter, there $way way(s)")
+                ways += way
+            }
+            dp[jolt] = ways
+        }
+        return dp[0] ?: -1
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day10Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day10Test.kt
@@ -7,13 +7,60 @@ internal class Day10Test {
 
     val puzzleInput = InputUtils.getLines(2020, 10)
 
+    val example = """16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4""".splitToSequence("\n")
+
+    val bigExample = """28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3""".splitToSequence("\n")
+
     @Test
     fun solve1() {
-        Day10.solve1(puzzleInput)
+        println(Day10.solve1(example))
+        println(Day10.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        Day10.solve2(puzzleInput)
+        println(Day10.solve2(example))
+//        println(Day10.solve2(bigExample))
+//        println(Day10.solve2(puzzleInput))
     }
 }
