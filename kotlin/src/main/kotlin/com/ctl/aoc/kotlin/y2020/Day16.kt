fd2cfab79a590f5c873d072cf49d116f092f8a19
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-16 07:44:16 +0000

    2020 Day 16

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day16.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day16.kt
@@ -1,11 +1,107 @@
 package com.ctl.aoc.kotlin.y2020
 
 object Day16 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    fun solve1(input: String): Int {
+        val (_, otherTickets, rules) = Input.parse(input)
+        return otherTickets.flatMap { it.values }
+                .filter { value -> !rules.any { it.valueAllowed(value) } }
+                .sum()
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    fun solve2(input: String): Long {
+        val (myTicket, otherTickets, rules) = Input.parse(input)
+
+        val validTickets = otherTickets.filter { ticket -> ticket.isValid(rules) }
+
+        val initialState = SearchState.build(rules, validTickets)
+        val finalState = initialState.search()
+
+        val fieldValues = finalState.fields.map { it as Field.Known }.mapIndexed { idx, f -> f.name to myTicket.values[idx] }.toMap()
+        println(fieldValues)
+        return fieldValues.filterKeys { it.contains("departure") }.map { it.value.toLong() }.fold(1L) { acc, n -> acc * n }
     }
+
+    data class SearchState(val fields: List<Field>) {
+        companion object {
+            fun build(rules: List<Rule>, validTickets: List<Ticket>): SearchState {
+                val allNames = rules.map { it.fieldName }
+                val rulesByName = rules.map { it.fieldName to it }.toMap()
+                val initialFields = validTickets.first().values.map { Field.Unknown(allNames) }
+                val newFields = initialFields.mapIndexed { idx, field ->
+                    val actualValues = validTickets.map { it.values[idx] }
+                    val filtered = field.possibleNames.filter { ruleName ->
+                        val rule = rulesByName[ruleName] ?: error("")
+                        actualValues.all { rule.valueAllowed(it) }
+                    }
+                    Field.Unknown(filtered)
+                }
+                return SearchState(newFields)
+            }
+        }
+    }
+
+    private tailrec fun SearchState.search(): SearchState {
+        val reduced = fields.map { field ->
+            when (field) {
+                is Field.Unknown -> if (field.possibleNames.size == 1) Field.Known(field.possibleNames.first()) else field
+                is Field.Known -> field
+            }
+        }
+        val knownFields = reduced.filterIsInstance(Field.Known::class.java).map { it.name }.toSet()
+        val newFields = reduced.map { field ->
+            when (field) {
+                is Field.Known -> field
+                is Field.Unknown -> Field.Unknown(field.possibleNames.filter { !knownFields.contains(it) })
+            }
+        }
+        return if (newFields == fields) {
+            if (fields.any { it is Field.Unknown }) {
+                throw Error("Unresolved $fields")
+            }
+            return this
+        } else {
+            this.copy(fields = newFields).search()
+        }
+    }
+
+    sealed class Field {
+        data class Known(val name: String) : Field()
+        data class Unknown(val possibleNames: List<String>) : Field()
+    }
+
+    data class Ticket(val values: List<Int>) {
+        fun isValid(rules: List<Rule>): Boolean {
+            return values.all { value -> rules.any { it.valueAllowed(value) } }
+        }
+    }
+
+    data class Rule(val fieldName: String, val ranges: List<IntRange>) {
+        fun valueAllowed(value: Int): Boolean = ranges.any { it.contains(value) }
+
+        companion object {
+            val regex = """([\w ]+): ([\d]+)-([\d]+) or ([\d]+)-([\d]+)""".toRegex()
+            fun parse(s: String): Rule {
+                val m = regex.matchEntire(s) ?: throw Error(s)
+                val values = m.groupValues
+                val ranges = listOf(values[2].toInt()..values[3].toInt(), values[4].toInt()..values[5].toInt())
+                return Rule(values[1], ranges)
+            }
+        }
+    }
+
+    data class Input(val myTicket: Ticket, val otherTickets: List<Ticket>, val rules: List<Rule>) {
+        companion object {
+            fun parse(input: String): Input {
+                val parts = input.split("\n\n").toList()
+                val rules = parts[0].split("\n").map { Rule.parse(it) }
+
+                val myTicket = Ticket(parts[1].split("\n")[1].split(",").map { it.toInt() })
+
+                val otherTickets = parts[2].split("\n").drop(1).map { Ticket(it.split(",").map { s -> s.toInt() }) }
+                return Input(myTicket, otherTickets, rules)
+            }
+        }
+    }
+
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day16Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day16Test.kt
@@ -5,15 +5,28 @@ import org.junit.jupiter.api.Test
 
 internal class Day16Test {
 
-    val puzzleInput = InputUtils.getLines(2020, 16)
+    val puzzleInput = InputUtils.getString(2020, 16)
+
+    val example = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9"""
 
     @Test
     fun solve1() {
-        Day16.solve1(puzzleInput)
+        println(Day16.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        Day16.solve2(puzzleInput)
+//        println(Day16.solve2(example))
+        println(Day16.solve2(puzzleInput))
     }
 }
