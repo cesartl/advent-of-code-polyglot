b03e9371004a7e2c62e32df2366ee0e693e50b16
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-18 07:39:05 +0000

    2020 Day 18 p1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day18.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day18.kt
@@ -1,11 +1,81 @@
 package com.ctl.aoc.kotlin.y2020
 
 object Day18 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: Sequence<String>): Long {
+        return input.map { evaluateExpression(it) }.sum()
     }
 
     fun solve2(input: Sequence<String>): Int {
         TODO()
+    }
+
+    sealed class Operator {
+        object Plus : Operator()
+        object Mult : Operator()
+
+        fun compute(x: Long, y: Long): Long {
+            return when (this) {
+                Plus -> x + y
+                Mult -> x * y
+            }
+        }
+    }
+
+    data class Evaluation(val acc: Long = 0L, val operator: Operator = Operator.Plus) {
+        fun eval(n: Long): Evaluation {
+            return this.copy(acc = operator.compute(acc, n))
+        }
+    }
+
+    fun evaluateExpression(string: String): Long {
+        val expression = string.replace(" ", "")
+//        println("Doing $expression")
+        var idx = 0
+        var evaluation = Evaluation()
+        while (idx < expression.length) {
+            val c = expression[idx]
+            evaluation = when {
+                c == '+' -> {
+                    idx++
+                    evaluation.copy(operator = Operator.Plus)
+                }
+                c == '*' -> {
+                    idx++
+                    evaluation.copy(operator = Operator.Mult)
+                }
+                c.isDigit() -> {
+                    idx++
+                    val n = c.toString().toLong()
+                    evaluation.eval(n)
+                }
+                c == '(' -> {
+                    val endIdx = findClosingBracket(expression, idx)
+                    val bracketExpression = expression.substring((idx + 1) until (endIdx - 1))
+                    val n = evaluateExpression(bracketExpression)
+                    idx = endIdx
+                    evaluation.eval(n)
+                }
+                else -> {
+                    throw Error("Char: $c at $idx")
+                }
+            }
+        }
+//        println("$expression is ${evaluation.acc}")
+        return evaluation.acc
+    }
+
+    fun findClosingBracket(s: String, start: Int): Int {
+        var bracketCount = 1
+        var idx = start + 1
+        while (bracketCount > 0) {
+            val c = s[idx]
+            if (c == '(') {
+                bracketCount++
+            } else if (c == ')') {
+                bracketCount--
+            }
+            idx++
+        }
+        return idx
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day18Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day18Test.kt
@@ -1,19 +1,35 @@
 package com.ctl.aoc.kotlin.y2020
 
 import com.ctl.aoc.kotlin.utils.InputUtils
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day18Test {
 
     val puzzleInput = InputUtils.getLines(2020, 18)
 
+
+    @Test
+    internal fun eval() {
+//        assertEquals(71, Day18.evaluateExpression("1 + 2 * 3 + 4 * 5 + 6"))
+//        assertEquals(51, Day18.evaluateExpression("1 + (2 * 3) + (4 * (5 + 6))"))
+//        println(Day18.evaluateExpression("(2+4*9)*(6+9*8+6)+6"))
+        assertEquals(13632, Day18.evaluateExpression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
+    }
+
+    @Test
+    internal fun findBracket() {
+        val s = "(4 * (5 + 6))"
+        println(Day18.findClosingBracket(s, 5))
+    }
+
     @Test
     fun solve1() {
-        Day18.solve1(puzzleInput)
+        println(Day18.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        Day18.solve2(puzzleInput)
+        println(Day18.solve2(puzzleInput))
     }
 }
