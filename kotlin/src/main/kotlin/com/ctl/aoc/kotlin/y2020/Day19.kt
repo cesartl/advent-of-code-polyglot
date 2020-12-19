e44e8b1e909939493b16ef3d3088239fc4c9cb19
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-19 08:42:52 +0000

    2020 Day 19 p1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day19.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day19.kt
@@ -1,11 +1,88 @@
 package com.ctl.aoc.kotlin.y2020
 
 object Day19 {
-    fun solve1(input: Sequence<String>): Int {
+
+    fun solve1(input: String): Int {
+        val (rules, messages) = Input.parse(input)
+
+        val rulesById = rules.map { it.id to it }.toMap()
+
+        val rule0 = evaluateRule(rulesById[0] ?: error(""), mutableMapOf(), rulesById)
+        return messages.count { rule0.patternsSet.contains(it) }
+    }
+
+    fun solve2(input: String): Int {
         TODO()
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    sealed class Rule {
+        abstract val id: Int
+
+        data class FixedRule(override val id: Int, val r: String) : Rule()
+        data class SubRule(override val id: Int, val subRules: List<List<Int>>) : Rule()
+    }
+
+    data class EvaluatedRule(val id: Int, val patterns: List<String>) {
+        val patternsSet by lazy {
+            patterns.toSet()
+        }
+    }
+
+
+    private fun evaluateRule(rule: Rule, evaluatedRules: MutableMap<Int, EvaluatedRule>, rulesById: Map<Int, Rule>): EvaluatedRule {
+        val cached = evaluatedRules[rule.id]
+        if (cached != null) {
+            return cached
+        }
+        val evaluated = when (rule) {
+            is Rule.FixedRule -> {
+                EvaluatedRule(rule.id, listOf(rule.r))
+            }
+            is Rule.SubRule -> {
+                val subRules = rule.subRules.map { ruleIds ->
+                    ruleIds.map {
+                        val subRule = rulesById[it] ?: error("Could not find rule id $it")
+                        evaluateRule(subRule, evaluatedRules, rulesById)
+                    }
+                }
+                val patterns = subRules.flatMap { allPatterns(subRules = it).toList() }
+                EvaluatedRule(rule.id, patterns)
+            }
+        }
+        evaluatedRules[evaluated.id] = evaluated
+        return evaluated
+    }
+
+    fun allPatterns(prefix: String = "", subRules: List<EvaluatedRule>): Sequence<String> = sequence {
+        if (subRules.isEmpty()) {
+            yield(prefix)
+        } else {
+            val rule = subRules.first()
+            rule.patterns.forEach { pattern ->
+                yieldAll(allPatterns(prefix + pattern, subRules.drop(1)))
+            }
+
+        }
+    }
+
+    data class Input(val rules: List<Rule>, val messages: List<String>) {
+        companion object {
+            fun parse(input: String): Input {
+                val s = input.split("\n\n")
+
+                val rules: List<Rule> = s[0].split("\n").map { line ->
+                    val p1 = line.split(":")
+                    val ruleId = p1[0].toInt()
+                    if (p1[1].contains('"')) {
+                        val r = p1[1].substringAfter('"').substringBefore('"')
+                        Rule.FixedRule(ruleId, r)
+                    } else {
+                        val subRules = p1[1].trim().split("|").map { s -> s.trim().split(" ").map { it.toInt() } }
+                        Rule.SubRule(ruleId, subRules)
+                    }
+                }
+                return Input(rules, s[1].split("\n"))
+            }
+        }
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day19Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2020/Day19Test.kt
@@ -5,15 +5,30 @@ import org.junit.jupiter.api.Test
 
 internal class Day19Test {
 
-    val puzzleInput = InputUtils.getLines(2020, 19)
+    val puzzleInput = InputUtils.getString(2020, 19)
+
+    val example = """0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb"""
+
 
     @Test
     fun solve1() {
-        Day19.solve1(puzzleInput)
+        println(Day19.solve1(example))
+        println(Day19.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        Day19.solve2(puzzleInput)
+        println(Day19.solve2(puzzleInput))
     }
 }
