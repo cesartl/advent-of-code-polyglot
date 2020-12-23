b07151ea26965d05512d4bc27b5b79938e51f836
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-23 10:04:23 +0000

    2020 Day 23 p2

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day23.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2020/Day23.kt
@@ -9,10 +9,8 @@ object Day23 {
         var current = CircularLinkedList.of(numbers.first())
         numbers.drop(1).reversed().forEach { current.insert(it) }
         val max = 9
-        current = play(n, current, max)
-        while (current.value != 1) {
-            current = current.nextNode()
-        }
+        val (finalNode, cache) = play(n, current, max)
+        current = cache[1]!!
         current = current.nextNode()
         var result = mutableListOf<Int>()
         (0 until numbers.size - 1).forEach {
@@ -22,7 +20,7 @@ object Day23 {
         return result.joinToString("")
     }
 
-    fun solve2(input: String): Int {
+    fun solve2(input: String): Long {
         val numbers = input.map { it.toString().toInt() }
         var current = CircularLinkedList.of(numbers.first())
         numbers.drop(1).reversed().forEach { current.insert(it) }
@@ -30,43 +28,60 @@ object Day23 {
         generateSequence(1000000) { it - 1 }.take(1000000 - numbers.size).forEach {
             last.insert(it)
         }
-        play(10000000, current, 1000000)
-        TODO()
+        val (finalNode, cache) = play(10000000, current, 1000000)
+        val one = cache[1]!!
+        return one.nextNode().value.toLong() * one.nextNode().nextNode().value.toLong()
     }
 
 
-    private fun play(n: Int, current: CircularLinkedList<Int>, max: Int): CircularLinkedList<Int> {
-        var current1 = current
+    private fun play(n: Int, circular: CircularLinkedList<Int>, max: Int): Pair<CircularLinkedList<Int>, MutableMap<Int, CircularLinkedList<Int>>> {
+        var current = circular
         var searching = 0L
         var startTime = System.currentTimeMillis()
+        var previous: CircularLinkedList<Int>
+        var next: CircularLinkedList<Int>
+        val cache = mutableMapOf<Int, CircularLinkedList<Int>>()
+
+        var toCache = current
+        while (cache.size < max) {
+            cache[toCache.value] = toCache
+            toCache = toCache.nextNode()
+        }
         (0 until n).forEach { i ->
-            if (i % 1000 == 0) {
+            if (i % 100000 == 0) {
                 println("i: $i, spent searching: $searching ms / ${System.currentTimeMillis() - startTime})")
             }
-            //            println(current.print())
-            val toMove = listOf(current1.removeNext(), current1.removeNext(), current1.removeNext())
-            var target = if (current1.value == 1) max else (current1.value - 1)
+//            println(current.print())
+            val toMove = listOf(current.removeNext(), current.removeNext(), current.removeNext())
+            toMove.forEach { cache.remove(it) }
+            var target = if (current.value == 1) max else (current.value - 1)
             while (toMove.contains(target)) {
                 target--
                 if (target <= 0) {
                     target = max
                 }
             }
-            val start = current1
 //            println("Looking for $target")
             var count = 0
             searching -= System.currentTimeMillis()
-            while (current1.value != target) {
-                current1 = current1.previousNode()
-                count++
-            }
+
+//            previous = current
+//            next = current
+//            while ((previous.value != target) && (next.value != target)) {
+//                next = next.nextNode()
+//                previous = previous.previousNode()
+//                count++
+//            }
+//            val toInsert = if(previous.value == target) previous else next
+            val toInsert = cache[target]!!
+
             searching += System.currentTimeMillis()
-//            println("Found after $count count")
+
             toMove.reversed().forEach {
-                current1.insert(it)
+                cache[it] = toInsert.insert(it)
             }
-            current1 = start.nextNode()
+            current = current.nextNode()
         }
-        return current1
+        return current to cache
     }
 }
