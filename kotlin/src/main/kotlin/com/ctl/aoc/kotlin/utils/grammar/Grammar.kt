a1b2e9edb9928651d1bb579752f88bbe8f72a54e
Cesar TL <cesar.tronlozai@gmai.com>
2020-12-29 16:01:09 +0000

    Earley Parser - DSL

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/utils/grammar/Grammar.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/utils/grammar/Grammar.kt
@@ -12,3 +12,54 @@ sealed class TerminalElement : GrammarElement()
 data class SingleChar(val char: Char) : TerminalElement()
 data class Range(val charRange: CharRange) : TerminalElement()
 data class CharClass(val allowedChars: Set<Char>) : TerminalElement()
+
+class RuleBuilder {
+    val elements = mutableListOf<GrammarElement>()
+
+    fun ref(name: String) {
+        elements.add(RuleRef(name))
+    }
+
+    operator fun String.not() {
+        ref(this)
+    }
+
+    fun char(char: Char) {
+        elements.add(SingleChar(char))
+    }
+
+    fun chars(vararg chars: Char) {
+        elements.add(CharClass(chars.toSet()))
+    }
+
+    operator fun Char.unaryPlus(){
+        elements.add(SingleChar(this))
+    }
+
+    operator fun CharRange.unaryPlus(){
+        elements.add(Range(this))
+    }
+}
+
+class GrammarBuilder {
+    private val rules = mutableListOf<GrammarRule>()
+    infix fun String.`=`(init: RuleBuilder.() -> Unit) {
+        val ruleBuilder = RuleBuilder()
+        ruleBuilder.init()
+        rules.add(GrammarRule(this, ruleBuilder.elements))
+    }
+
+    infix fun String.`=`(rule: String) {
+        rules.add(GrammarRule(this, listOf(RuleRef(rule))))
+    }
+
+    fun build(): Grammar {
+        return Grammar(rules.first().name, rules)
+    }
+}
+
+fun grammar(init: GrammarBuilder.() -> Unit): GrammarBuilder {
+    val builder = GrammarBuilder()
+    builder.init()
+    return builder
+}
--- kotlin/src/test/java/com/ctl/aoc/kotlin/utils/grammar/EarleyItemTest.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/utils/grammar/EarleyItemTest.kt
@@ -3,7 +3,7 @@ package com.ctl.aoc.kotlin.utils.grammar
 import org.junit.jupiter.api.Test
 
 internal class EarleyItemTest {
-    val simpleGrammar = Grammar(
+    private val simpleGrammar = Grammar(
             startRuleName = "Sum",
             rules = listOf(
                     GrammarRule("Sum", listOf(RuleRef("Sum"), CharClass(setOf('+', '-')), RuleRef("Product"))),
@@ -17,13 +17,31 @@ internal class EarleyItemTest {
             )
     )
 
+    private val grammar = grammar {
+        "Sum" `=` { !"Sum"; chars('+', '-'); !"Product" }
+        "Sum" `=` "Product"
+        "Product" `=` { !"Product"; chars('*', '/'); !"Factor" }
+        "Product" `=` "Factor"
+        "Factor" `=` { +'('; !"Sum"; +')' }
+        "Factor" `=` "Number"
+        "Number" `=` { +('0'..'9'); !"Number" }
+        "Number" `=` { +('0'..'9') }
+    }.build()
+
+    val cesar: Int.(Int) -> Int = { other -> plus(other) }
+
+    @Test
+    internal fun cesar() {
+        println(123.cesar(5))
+    }
+
     @Test
     internal fun verifyTest() {
         simpleGrammar.run {
             println(earleyMatch("1+(2*3+4)"))
             println(earleyMatch("1+(2%3+4)"))
         }
-        println("1+(2*3+4)".matches(simpleGrammar))
-        println("1+(2%3+4)".matches(simpleGrammar))
+        println("1+(2*3+4)".matches(grammar))
+        println("1+(2%3+4)".matches(grammar))
     }
 }
