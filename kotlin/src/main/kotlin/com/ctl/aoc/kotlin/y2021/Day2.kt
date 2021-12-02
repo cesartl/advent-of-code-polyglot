046d2890fd547612eeb14a74eb5a34e7096ea7e3
Cesar TL <cesar.tronlozai@gmai.com>
2021-12-02 07:50:32 +0000

    y2021 day2

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day2.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day2.kt
@@ -1,11 +1,62 @@
 package com.ctl.aoc.kotlin.y2021
 
+import com.ctl.aoc.kotlin.utils.Position
+
 object Day2 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    sealed class Command {
+        data class Forward(val n: Int) : Command()
+        data class Down(val n: Int) : Command()
+        data class Up(val n: Int) : Command()
+
+        companion object {
+            fun parse(s: String): Command {
+                val parts = s.split(" ")
+                return when {
+                    s.startsWith("forward") -> {
+                        Forward(parts[1].toInt())
+                    }
+                    s.startsWith("down") -> {
+                        Down(parts[1].toInt())
+                    }
+                    s.startsWith("up") -> {
+                        Up(parts[1].toInt())
+                    }
+                    else -> error(s)
+                }
+            }
+        }
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun Position.apply(command: Command): Position {
+        return when (command) {
+            is Command.Down -> this + Position(0, -1).scalar(command.n)
+            is Command.Up -> this + Position(0, 1).scalar(command.n)
+            is Command.Forward -> this + Position(1, 0).scalar(command.n)
+        }
+    }
+
+    data class Submarine(val x: Long, val depth: Long, val aim: Long)
+
+    private fun Submarine.apply(command: Command): Submarine {
+        return when (command) {
+            is Command.Down -> this.copy(aim = this.aim + command.n)
+            is Command.Up -> this.copy(aim = this.aim - command.n)
+            is Command.Forward -> this.copy(x = this.x + command.n, depth = this.depth + this.aim * command.n)
+        }
+    }
+
+    fun solve1(input: Sequence<String>): Int {
+        val end = input.map { Command.parse(it) }
+                .fold(Position(0, 0)) { p, command -> p.apply(command) }
+        println(end)
+        return (-end.y) * end.x
+    }
+
+    fun solve2(input: Sequence<String>): Long {
+        val end = input.map { Command.parse(it) }
+                .fold(Submarine(0, 0, 0)) { p, command -> p.apply(command) }
+        println(end)
+        return end.x * end.depth
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day2Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day2Test.kt
@@ -7,13 +7,22 @@ internal class Day2Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2021, 2)
 
+    val example = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day2.solve1(example))
         println(Day2.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day2.solve2(example))
         println(Day2.solve2(puzzleInput))
     }
 }
