a8be216b47ae5b8eef4cabd816342d52e542f090
Cesar TL <cesar.tronlozai@gmai.com>
2021-12-14 07:22:53 +0000

    y2021 day14p1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day14.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day14.kt
@@ -1,11 +1,65 @@
 package com.ctl.aoc.kotlin.y2021
 
+import com.ctl.aoc.kotlin.utils.frequency
+
 object Day14 {
+
+    @JvmInline
+    value class Polymer(val poly: String)
+
+    data class Rule(val pattern: String, val output: String) {
+        val regex = pattern.toRegex()
+        fun findAllMatches(poly: String): Sequence<Int> = sequence {
+            yieldAll(findOneIndex(0, poly))
+        }
+
+        fun findOneIndex(start: Int, poly: String): Sequence<Int> = sequence {
+//            println("start $start: ${poly.drop(start)}")
+            val i = poly.indexOf(string = pattern, startIndex = start)
+//            println("i $i")
+            if (i >= 0) {
+                yield(i)
+                yieldAll(findOneIndex(i + 1, poly))
+            }
+        }
+
+    }
+
+    fun String.insertAt(i: Int, s: String): String {
+        return this.slice(0 until i) + s + this.drop(i)
+    }
+
+    fun Polymer.applyRules(rules: List<Rule>): Polymer {
+        val matches = rules.flatMap { rule ->
+            rule.findAllMatches(poly).map { rule to it }
+        }.sortedBy { it.second }
+        val newPoly = matches.foldIndexed(poly) { index, acc, (rule, ruleIdx) ->
+            acc.insertAt(index + ruleIdx + 1, rule.output)
+        }
+        return Polymer(newPoly)
+    }
+
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val (polymer, rules) = parse(input)
+        val result = generateSequence(polymer) { it.applyRules(rules) }
+                .drop(10)
+                .first()
+        val frequency = result.poly.toList().frequency()
+        return frequency.maxByOrNull { it.value }!!.value - frequency.minByOrNull { it.value }!!.value
     }
 
     fun solve2(input: Sequence<String>): Int {
         TODO()
+    }
+
+    fun parse(lines: Sequence<String>): Pair<Polymer, List<Rule>> {
+        val polymer = Polymer(lines.first())
+        val rules = lines.drop(2)
+                .map { line ->
+                    val parts = line.split(" -> ")
+                    Rule(parts[0], parts[1])
+                }
+                .toList()
+        return polymer to rules
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day14Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day14Test.kt
@@ -1,14 +1,48 @@
 package com.ctl.aoc.kotlin.y2021
 
 import com.ctl.aoc.kotlin.utils.InputUtils
+import com.ctl.aoc.kotlin.y2021.Day14.insertAt
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day14Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2021, 14)
 
+    val example = """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C""".splitToSequence("\n")
+
+    @Test
+    internal fun testInsert() {
+        assertEquals("ABC", "AC".insertAt(1, "B"))
+    }
+
+    @Test
+    fun findALl() {
+        val rule = Day14.Rule("BB", "C")
+        assertEquals(listOf(5, 6, 12), rule.findAllMatches("NBCCNBBBCBHCBB").toList())
+//        assertEquals(listOf(0), Day14.Rule("NN", "").findAllMatches("NNCB").toList())
+    }
+
     @Test
     fun solve1() {
+        println(Day14.solve1(example))
         println(Day14.solve1(puzzleInput))
     }
 
