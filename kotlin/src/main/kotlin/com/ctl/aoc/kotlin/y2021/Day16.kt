2f94213cc9d37ebdf808c90ac7fafa9d563993d9
Cesar TL <cesar.tronlozai@gmai.com>
2021-12-16 11:54:15 +0000

    y2021 day16

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day16.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day16.kt
@@ -1,11 +1,142 @@
 package com.ctl.aoc.kotlin.y2021
 
 object Day16 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    @JvmInline
+    value class BinaryString(val binary: String = "") {
+        val length: Int
+            get() = binary.length
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    fun BinaryString.toLong(): Long = binary.toLong(2)
+    fun BinaryString.startsWith(c: Char): Boolean = binary.startsWith(c)
+    operator fun BinaryString.plus(other: BinaryString): BinaryString = BinaryString(this.binary + other.binary)
+    fun BinaryString.drop(n: Long) = BinaryString(binary.drop(1))
+
+    fun String.toBinary(): BinaryString = this.splitToSequence("").filter { it != "" }.joinToString(separator = "") { Integer.toBinaryString(Integer.parseInt(it, 16)).padStart(4, '0') }.run { BinaryString(this) }
+
+    @JvmInline
+    value class Version(val binary: BinaryString)
+
+    @JvmInline
+    value class TypeId(val binary: BinaryString) {
+        val type: Long
+            get() = binary.toLong()
+    }
+
+    sealed class Packet {
+        abstract val version: Version
+        abstract val typeId: TypeId
+        abstract val source: BinaryString
+
+        data class Literal(val literalValue: BinaryString, override val version: Version, override val typeId: TypeId, override val source: BinaryString) : Packet() {
+            val numeric: Long = literalValue.toLong()
+            override fun toString(): String {
+                return "Literal(source=$source, numeric=$numeric)"
+            }
+
+        }
+
+        data class Operator(val subPackets: List<Packet>, override val version: Version, override val typeId: TypeId, override val source: BinaryString) : Packet() {
+            override fun toString(): String {
+                return "Operator(subPackets=$subPackets, typeId=$typeId)"
+            }
+        }
+    }
+
+    fun Packet.versionSum(): Long {
+        return when (this) {
+            is Packet.Literal -> this.version.binary.toLong()
+            is Packet.Operator -> this.version.binary.toLong() + subPackets.sumOf { it.versionSum() }
+        }
+    }
+
+    fun Packet.run(): Long {
+        return when (this) {
+            is Packet.Literal -> this.numeric
+            is Packet.Operator -> {
+                when (this.typeId.type) {
+                    0L -> this.subPackets.sumOf { it.run() }
+                    1L -> this.subPackets.map { it.run() }.fold(1L) { x, y -> x * y }
+                    2L -> this.subPackets.minOf { it.run() }
+                    3L -> this.subPackets.maxOf { it.run() }
+                    5L -> if (this.subPackets[0].run() > this.subPackets[1].run()) 1 else 0
+                    6L -> if (this.subPackets[0].run() < this.subPackets[1].run()) 1 else 0
+                    7L -> if (this.subPackets[0].run() == this.subPackets[1].run()) 1 else 0
+                    else -> error("unknown type ${this.typeId.type}")
+                }
+            }
+        }
+    }
+
+    class Scanner(private val source: BinaryString) {
+        private var start = 0
+        private var current = 0
+
+        fun scanPacket(): Packet {
+            return parsePacket()
+        }
+
+        private fun parsePacket(): Packet {
+            start = current
+            val version = parseVersion()
+            val typeId = parseTypeId()
+            return if (typeId.type == 4L) {
+                parseLiteral(version, typeId)
+            } else {
+                parseOperator(version, typeId)
+            }
+        }
+
+        private fun parseLiteral(version: Version, typeId: TypeId): Packet.Literal {
+            var group: BinaryString
+            var literalValue = BinaryString()
+            do {
+                group = advance(5)
+                literalValue += group.drop(1)
+            } while (group.startsWith('1'))
+            val packetSource = BinaryString(source.binary.substring(start, current))
+            return Packet.Literal(literalValue, version, typeId, packetSource)
+        }
+
+        private fun parseOperator(version: Version, typeId: TypeId): Packet.Operator {
+            val operatorStart = start
+            val mode = advance()
+            if (mode == '0') {
+                val nBits = advance(15).toLong()
+                val subPackets = mutableListOf<Packet>()
+                while (subPackets.sumOf { it.source.length } < nBits.toLong()) {
+                    subPackets.add(parsePacket())
+                }
+                val packetSource = BinaryString(source.binary.substring(operatorStart, current))
+                return Packet.Operator(subPackets, version, typeId, packetSource)
+            } else {
+                val nPackets = advance(11).toLong()
+                val subPackets = mutableListOf<Packet>()
+                while (subPackets.size < nPackets) {
+                    subPackets.add(parsePacket())
+                }
+                val packetSource = BinaryString(source.binary.substring(operatorStart, current))
+                return Packet.Operator(subPackets, version, typeId, packetSource)
+            }
+        }
+
+        fun parseVersion(): Version = Version(advance(3))
+        fun parseTypeId(): TypeId = TypeId(advance(3))
+
+        private fun advance(): Char = source.binary[current++]
+        private fun advance(n: Int): BinaryString = generateSequence(advance()) { advance() }.take(n).joinToString(separator = "").run { BinaryString(this) }
+    }
+
+    fun BinaryString.parse(): Packet {
+        return Scanner(this).scanPacket()
+    }
+
+    fun solve1(input: String): Long {
+        return input.toBinary().parse().versionSum()
+    }
+
+    fun solve2(input: String): Long {
+        return input.toBinary().parse().run()
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day16Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day16Test.kt
@@ -1,11 +1,28 @@
 package com.ctl.aoc.kotlin.y2021
 
 import com.ctl.aoc.kotlin.utils.InputUtils
+import com.ctl.aoc.kotlin.y2021.Day16.parse
+import com.ctl.aoc.kotlin.y2021.Day16.toBinary
+import com.ctl.aoc.kotlin.y2021.Day16.versionSum
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day16Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2021, 16)
+    val puzzleInput = InputUtils.downloadAndGetString(2021, 16)
+
+    @Test
+    internal fun testBinary() {
+        assertEquals("110100101111111000101000", "D2FE28".toBinary())
+        assertEquals("00111000000000000110111101000101001010010001001000000000", "38006F45291200".toBinary())
+    }
+
+    @Test
+    internal fun testPacket() {
+        println("EE00D40C823060".toBinary().parse())
+        assertEquals(16, "8A004A801A8002F478".toBinary().parse().versionSum())
+        assertEquals(31, "A0016C880162017C3686B18A3D4780".toBinary().parse().versionSum())
+    }
 
     @Test
     fun solve1() {
@@ -14,6 +31,9 @@ internal class Day16Test {
 
     @Test
     fun solve2() {
+        println(Day16.solve2("9C0141080250320F1802104A08"))
         println(Day16.solve2(puzzleInput))
     }
+
+
 }
