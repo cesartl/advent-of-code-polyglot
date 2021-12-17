2527e7da54604da2f7ea96c02012a7b55071f4de
Cesar TL <cesar.tronlozai@gmai.com>
2021-12-17 11:14:23 +0000

    y2021 day17

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day17.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2021/Day17.kt
@@ -1,11 +1,75 @@
 package com.ctl.aoc.kotlin.y2021
 
+import com.ctl.aoc.kotlin.utils.Position
+
 object Day17 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    @JvmInline
+    value class Velocity(val vector: Position)
+
+    data class Target(val xRange: IntRange, val yRange: IntRange)
+
+    data class State(val position: Position = Position(0, 0), val velocity: Velocity)
+
+    fun State.next(): State {
+        val (x, y) = position
+        val (vx, vy) = velocity.vector
+        return State(Position(x + vx, y + vy), Velocity(vector = Position((vx - 1).coerceAtLeast(0), vy - 1)))
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun Position.isWithin(target: Target): Boolean {
+        val (x, y) = this
+        return x in target.xRange && y in target.yRange
+    }
+
+    private fun Position.isOut(target: Target): Boolean {
+        val (x, y) = this
+        return x > target.xRange.last || y < target.yRange.first
+    }
+
+    private fun State.launch(): Sequence<State> = generateSequence(this) { it.next() }
+
+    @JvmInline
+    value class Trajectory(val positions: List<Position>)
+
+    private fun Velocity.aim(target: Target): Trajectory? {
+        val start = State(velocity = this)
+        val trajectory = start.launch().takeWhile { !it.position.isOut(target) }
+        return if (trajectory.any { it.position.isWithin(target) }) Trajectory(trajectory.map { it.position }.toList()) else null
+    }
+
+    private fun Target.allTrajectories(xRange: IntRange, yRange: IntRange): Sequence<Trajectory> {
+        val target = this
+        return sequence {
+            xRange.forEach { x ->
+                yRange.forEach { y ->
+                    Velocity(Position(x, y)).aim(target)?.let { yield(it) }
+                }
+            }
+        }
+    }
+
+    fun Trajectory.maxY(): Int = positions.maxOf { it.y }
+
+    fun solve1(input: String): Int {
+        val target = input.parseTarget()
+        val all = target.allTrajectories(0..target.xRange.last, -500..500).toList()
+        val best = all.maxByOrNull { it.maxY() }!!
+        return best.maxY()
+    }
+
+    fun solve2(input: String): Int {
+        val target = input.parseTarget()
+        val all = target.allTrajectories(0..target.xRange.last, -500..500).toList()
+        return all.size
+    }
+
+    val regex = """target area: x=([-\d]+)..([-\d]+), y=([-\d]+)..([-\d]+)""".toRegex()
+    private fun String.parseTarget(): Target {
+        val match = regex.matchEntire(this) ?: error(this)
+        val groups = match.groupValues.drop(1).map { it.toInt() }
+        val xRange = (groups[0]..groups[1])
+        val yRange = (groups[2]..groups[3])
+        return Day17.Target(xRange, yRange)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day17Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2021/Day17Test.kt
@@ -5,15 +5,19 @@ import org.junit.jupiter.api.Test
 
 internal class Day17Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2021, 17)
+    val puzzleInput = InputUtils.downloadAndGetString(2021, 17)
+
+    val example = "target area: x=20..30, y=-10..-5"
 
     @Test
     fun solve1() {
+        println(Day17.solve1(example))
         println(Day17.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day17.solve2(example))
         println(Day17.solve2(puzzleInput))
     }
 }
