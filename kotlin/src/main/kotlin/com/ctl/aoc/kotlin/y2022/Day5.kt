aab35c07c4aa937ed89b5ed0b6905a5fe67e5d77
Cesar Tron-Lozai <ctron@thousandeyes.com>
2022-12-05 18:05:17 +0000

    2022 day5

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day5.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day5.kt
@@ -1,11 +1,68 @@
 package com.ctl.aoc.kotlin.y2022
 
 object Day5 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    private val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
+
+    data class Move(val n: Int, val from: Int, val to: Int)
+
+    private fun String.toMove(): Move = regex.matchEntire(this)?.let {
+        val n = it.groupValues[1].toInt()
+        val from = it.groupValues[2].toInt()
+        val to = it.groupValues[3].toInt()
+        Move(n, from, to)
+    } ?: error(this)
+
+    data class Crates(
+        val stacks: List<ArrayDeque<Char>> = (0..8).map { ArrayDeque() }
+    ) {
+        fun apply(move: Move) {
+            repeat(move.n) {
+                val c = stacks[move.from - 1].removeFirst()
+                stacks[move.to - 1].addFirst(c)
+            }
+        }
+
+        fun apply2(move: Move) {
+            val buffer = ArrayDeque<Char>()
+            repeat(move.n) {
+                val c = stacks[move.from - 1].removeFirst()
+                buffer.addFirst(c)
+            }
+            buffer.forEach { c ->
+                stacks[move.to - 1].addFirst(c)
+            }
+        }
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: Sequence<String>): String {
+        val crates = buildCrates(input)
+        input.drop(9).map { it.toMove() }.forEach {
+            crates.apply(it)
+        }
+        return crates.stacks.map { it.first() }.joinToString("")
+    }
+
+    fun solve2(input: Sequence<String>): String {
+        val crates = buildCrates(input)
+        input.drop(9).map { it.toMove() }.forEach {
+            crates.apply2(it)
+        }
+        return crates.stacks.map { it.first() }.joinToString("")
+    }
+
+    private fun buildCrates(input: Sequence<String>): Crates {
+        val crates = Crates()
+        input.take(8)
+            .map { line -> line.chunked(4).map { it.trim() } }
+            .forEach { elements ->
+                elements.forEachIndexed { crateNum, s ->
+                    if (s.isNotEmpty()) {
+                        val stack = crates.stacks[crateNum]
+                        stack.addLast(s[1])
+                    }
+                }
+            }
+        return crates
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/utils/RangesKtTest.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/utils/RangesKtTest.kt
@@ -1,6 +1,7 @@
 package com.ctl.aoc.kotlin.utils
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class RangesKtTest {
@@ -15,14 +16,5 @@ internal class RangesKtTest {
         assertFalse((1..3).includes(4..6))
         assertFalse((11..12).includes(12..20))
         assertFalse((4..6).includes(6..7))
-
-        assertTrue((1..4).includes2(2..4))
-        assertTrue((1..4).includes2(1..4))
-        assertTrue((4..6).includes2(6..6))
-        assertTrue((6..6).includes2(6..6))
-
-        assertFalse((1..3).includes2(4..6))
-        assertFalse((11..12).includes2(12..20))
-        assertFalse((4..6).includes2(6..7))
     }
 }
