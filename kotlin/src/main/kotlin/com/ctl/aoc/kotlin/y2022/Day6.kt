0f567aa3a45c51622da0acf923fb588411808fb9
Cesar Tron-Lozai <ctron@thousandeyes.com>
2022-12-06 09:37:26 +0000

    2022 day6

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day6.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day6.kt
@@ -1,11 +1,20 @@
 package com.ctl.aoc.kotlin.y2022
 
 object Day6 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: String): Int {
+        return findFirstDistinctN(input, 4)
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun findFirstDistinctN(input: String, n: Int): Int {
+        return input
+            .asSequence()
+            .windowed(size = n, step = 1)
+            .withIndex()
+            .first { (index, list) -> list.toSet().size == n }
+            .index + n
+    }
+
+    fun solve2(input: String): Int {
+        return findFirstDistinctN(input, 14)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2022/Day6Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2022/Day6Test.kt
@@ -1,21 +1,22 @@
 package com.ctl.aoc.kotlin.y2022
 
 import com.ctl.aoc.kotlin.utils.InputUtils
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 internal class Day6Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2022, 6)
+    val puzzleInput = InputUtils.downloadAndGetString(2022, 6)
 
     @Test
     fun solve1() {
+        assertEquals(7, Day6.solve1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
         println(Day6.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        assertEquals(19, Day6.solve2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
         println(Day6.solve2(puzzleInput))
     }
 }
