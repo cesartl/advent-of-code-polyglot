8b0e73325aa30fd6251c5eb43ff0f676f362e8dd
Cesar Tron-Lozai <ctron@thousandeyes.com>
2022-12-20 06:20:59 +0000

    2022 day20 wip

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day20.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day20.kt
@@ -1,8 +1,53 @@
 package com.ctl.aoc.kotlin.y2022
 
+import com.ctl.aoc.kotlin.utils.CircularLinkedList
+
 object Day20 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val list = input.map { it.toInt() }.toList()
+        val size = list.size
+        val circular = CircularLinkedList.of(list.first())
+        var insert = circular
+        val nodes = mutableListOf<CircularLinkedList<Int>>()
+        list.drop(1).forEach {
+            insert = insert.insert(it)
+            nodes.add(insert)
+        }
+        nodes.add(circular)
+        println(circular.print { "${it.value} " })
+        var current = circular
+        val iterator = nodes.iterator()
+        var first = current
+        repeat(list.size) {
+            if (current == first) {
+                first = current.nextNode()
+            }
+            val n = current.value
+            val mod = if (n >= 0) {
+                n % size
+            } else {
+                (2 * size + n - 1) % size
+            }
+//            println("Doing ${current.value} (mod=$mod)")
+            if (mod > 0) {
+                current = current.previousNode()
+                current.removeNext()
+                current = current.nextNode(mod)
+                current.insert(n)
+            } else if (mod < 0) {
+                error("n=$n mod = $mod")
+            }
+//            println("current: " + current.print { "${it.value} " })
+            current = iterator.next()
+        }
+        println(first.print { "${it.value} " })
+        var zero = current
+        while (zero.value != 0) {
+            zero = zero.nextNode()
+        }
+        return sequenceOf(1000, 2000, 3000)
+            .map { zero.nextNode(it % size).value }
+            .sum()
     }
 
     fun solve2(input: Sequence<String>): Int {
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2022/Day20Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2022/Day20Test.kt
@@ -3,15 +3,23 @@ package com.ctl.aoc.kotlin.y2022
 import com.ctl.aoc.kotlin.utils.InputUtils
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
 internal class Day20Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2022, 20)
 
+    val exampleInput = """1
+2
+-3
+3
+-2
+0
+4""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day20.solve1(exampleInput))
         println(Day20.solve1(puzzleInput))
+        //>2785
     }
 
     @Test
