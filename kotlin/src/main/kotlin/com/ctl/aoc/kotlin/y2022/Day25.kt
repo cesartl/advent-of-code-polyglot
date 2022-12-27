37fe024236cdbc5eb0c5c9cc425785bc61b3546d
Cesar Tron-Lozai <ctron@thousandeyes.com>
2022-12-27 15:09:38 +0100

    2022 day25

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day25.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2022/Day25.kt
@@ -1,8 +1,84 @@
 package com.ctl.aoc.kotlin.y2022
 
+fun String.toSnafu() = Day25.Snafu(this)
+
 object Day25 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    @JvmInline
+    value class Snafu(val digits: String) {
+        fun charFromEnd(i: Int): Char {
+            if (i >= digits.length) {
+                return '0'
+            }
+            return digits[digits.length - i - 1]
+        }
+    }
+
+    private val charValue = mapOf(
+        '=' to -2,
+        '-' to -1,
+        '0' to 0,
+        '1' to 1,
+        '2' to 2,
+    )
+
+    private val reverseCharValue = charValue.asSequence().associate { it.value to it.key }
+
+    operator fun Snafu.plus(other: Snafu): Snafu {
+        val max = this.digits.length.coerceAtLeast(other.digits.length) + 1
+        val buffer = CharArray(max)
+        var i = 0
+        var carry = 0
+        while (i < max) {
+            val a = this.charFromEnd(i)
+            val b = other.charFromEnd(i)
+            val sum = charValue[a]!! + charValue[b]!! + carry
+            if (sum in -2..2) {
+                buffer[buffer.size - 1 - i] = reverseCharValue[sum]!!
+                carry = 0
+            } else {
+                when (sum) {
+                    5 -> {
+                        buffer[buffer.size - 1 - i] = '0'
+                        carry = 1
+                    }
+
+                    4 -> {
+                        buffer[buffer.size - 1 - i] = '-'
+                        carry = 1
+                    }
+
+                    3 -> {
+                        buffer[buffer.size - 1 - i] = '='
+                        carry = 1
+                    }
+
+                    -3 -> {
+                        buffer[buffer.size - 1 - i] = '2'
+                        carry = -1
+                    }
+                    -4 -> {
+                        buffer[buffer.size - 1 - i] = '1'
+                        carry = -1
+                    }
+
+                    -5 ->{
+                        buffer[buffer.size - 1 - i] = '0'
+                        carry = -1
+                    }
+
+                    else -> error("sum: $sum")
+                }
+            }
+            i++
+        }
+        return Snafu(buffer.joinToString(separator = "").trim())
+    }
+
+    fun solve1(input: Sequence<String>): String {
+        return input.map { it.toSnafu() }
+            .fold("0".toSnafu()) { acc, snafu -> acc + snafu }
+            .digits
     }
 
     fun solve2(input: Sequence<String>): Int {
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2022/Day25Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2022/Day25Test.kt
@@ -1,16 +1,36 @@
 package com.ctl.aoc.kotlin.y2022
 
 import com.ctl.aoc.kotlin.utils.InputUtils
+import com.ctl.aoc.kotlin.y2022.Day25.plus
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 internal class Day25Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2022, 25)
 
+    val example = """1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122""".splitToSequence("\n")
+
+    @Test
+    internal fun plus() {
+        assertEquals("1=".toSnafu(), "1".toSnafu() + "2".toSnafu())
+    }
+
     @Test
     fun solve1() {
+        println(Day25.solve1(example))
         println(Day25.solve1(puzzleInput))
     }
 
