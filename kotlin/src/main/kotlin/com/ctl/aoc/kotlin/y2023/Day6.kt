edf0fc8bde3089ace862047d627cbd4f14283da7
Cesar Tron-Lozai <ctron@thousandeyes.com>
2023-12-07 18:58:59 +0000

    2023 day6

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day6.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day6.kt
@@ -1,11 +1,53 @@
 package com.ctl.aoc.kotlin.y2023
 
+data class Race(
+    val time: Int,
+    val distance: Long
+)
+
+private fun Race.holdButton(duration: Int): Long {
+    return duration * (time - duration).toLong()
+}
+
+private fun Race.options(): Sequence<Long> = sequence {
+    repeat(time) {
+        yield(holdButton(it))
+    }
+}
+
+private fun parseRaces(input: Sequence<String>): List<Race> {
+    val l = input.filter { it.isNotEmpty() }.toList()
+    val regex = """\s+""".toRegex()
+    val times = l[0].splitToSequence(regex).filter { it.isNotEmpty() }.drop(1).map { it.toInt() }
+    val distances = l[1].splitToSequence(regex).filter { it.isNotEmpty() }.drop(1).map { it.toLong() }
+    return times.zip(distances)
+        .map { (time, distance) -> Race(time, distance) }
+        .toList()
+}
+
+private fun parseRace(input: Sequence<String>): Race {
+    val l = input.filter { it.isNotEmpty() }.toList()
+    val regex = """\s+""".toRegex()
+    val time = l[0].splitToSequence(regex).filter { it.isNotEmpty() }.drop(1).joinToString(separator = "").toInt()
+    val distance = l[1].splitToSequence(regex).filter { it.isNotEmpty() }.drop(1).joinToString(separator = "").toLong()
+    return Race(time, distance)
+}
+
 object Day6 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: Sequence<String>): Long {
+        val races = parseRaces(input)
+        return races.asSequence()
+            .map { race -> race.options().filter { it > race.distance }.count().toLong() }
+            .fold(1L) { acc, l -> acc * l }
     }
 
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        val race = parseRace(input)
+        println(race)
+        return race
+            .options()
+            .dropWhile { it < race.distance }
+            .takeWhile { it > race.distance }
+            .count()
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day6Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day6Test.kt
@@ -6,14 +6,20 @@ import org.junit.jupiter.api.Test
 internal class Day6Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2023, 6)
+    val example = """
+        Time:      7  15   30
+        Distance:  9  40  200
+    """.splitToSequence("\n")
 
     @Test
     fun solve1() {
+        println(Day6.solve1(example))
         println(Day6.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day6.solve2(example))
         println(Day6.solve2(puzzleInput))
     }
 }
