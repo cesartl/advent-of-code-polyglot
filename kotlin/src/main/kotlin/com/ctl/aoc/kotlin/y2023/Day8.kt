1f737ecbfd534d40457a9bd608ef9e10eefa5ca6
Cesar Tron-Lozai <ctron@thousandeyes.com>
2023-12-09 00:20:07 +0000

    2023 day8 part1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day8.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day8.kt
@@ -1,11 +1,55 @@
 package com.ctl.aoc.kotlin.y2023
 
+data class HauntedMap(
+    val directions: String,
+    val nodes: Map<String, Pair<String, String>>
+) {
+    fun directions(): Sequence<Char> = generateSequence(directions) { it }
+        .flatMap { it.asSequence() }
+
+    fun next(current: String, direction: Char): String {
+        val node = nodes[current] ?: error("No nodes from $current")
+        return when (direction) {
+            'L' -> node.first
+            'R' -> node.second
+            else -> error("Unknown direction $direction")
+        }
+    }
+}
+
+private val regex = """(\w+) = \((\w+), (\w+)\)""".toRegex()
+private fun parseHauntedMap(input: Sequence<String>): HauntedMap {
+    val lines = input.filter { it.isNotEmpty() }.toList()
+    val direction = lines[0]
+    val nodes = mutableMapOf<String, Pair<String, String>>()
+    lines.asSequence()
+        .drop(1)
+        .forEach { line ->
+            val m = regex.matchEntire(line) ?: error(line)
+            val values = m.groupValues
+            nodes[values[1]] = values[2] to values[3]
+        }
+    return HauntedMap(
+        directions = direction,
+        nodes = nodes
+    )
+}
+
+private fun HauntedMap.walk(start: String): Sequence<String> {
+    return directions().runningFold(start) { current, direction -> next(current, direction) }
+}
+
 object Day8 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val map = parseHauntedMap(input)
+        return map.walk("AAA")
+            .withIndex()
+            .first { it.value == "ZZZ" }
+            .index
     }
 
     fun solve2(input: Sequence<String>): Int {
+        val map = parseHauntedMap(input)
         TODO()
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day8Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day8Test.kt
@@ -7,13 +7,36 @@ internal class Day8Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2023, 8)
 
+    val example = """RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)""".splitToSequence("\n")
+
+    val example2 = """RL
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day8.solve1(example))
         println(Day8.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day8.solve2(example2))
         println(Day8.solve2(puzzleInput))
     }
 }
