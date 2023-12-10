b4a3ee3889cd33ffd70fc7eb6d44d5b24ee3a9f5
Cesar Tron-Lozai <ctron@thousandeyes.com>
2023-12-10 14:07:19 +0000

    2023 day9

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day9.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day9.kt
@@ -1,11 +1,41 @@
 package com.ctl.aoc.kotlin.y2023
 
 object Day9 {
+
+    private fun generateDiffs(l: List<Int>): Sequence<List<Int>> {
+        return generateSequence(l) {
+            it.zipWithNext { a, b -> b - a }
+        }.takeWhile { list -> list.any { it != 0 } }
+    }
+
+    private fun extrapolateRight(l: List<Int>): Int {
+        return generateDiffs(l)
+            .map { it.last() }
+            .sum()
+    }
+
+    private fun extrapolateLeft(l: List<Int>): Int {
+        return generateDiffs(l)
+            .map { it.first() }
+            .toList()
+            .reversed()
+            .fold(0) { acc, i -> i - acc }
+    }
+
+
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        return input.map { line -> line.splitNumbers() }
+            .map { extrapolateRight(it) }
+            .sum()
     }
 
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        return input.map { line -> line.splitNumbers() }
+            .map { extrapolateLeft(it) }
+            .sum()
     }
+}
+
+private fun String.splitNumbers(character: String = " "): List<Int> {
+    return this.splitToSequence(character).filterNot { it.isBlank() }.map { it.toInt() }.toList()
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day9Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day9Test.kt
@@ -7,13 +7,19 @@ internal class Day9Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2023, 9)
 
+    val example = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day9.solve1(example))
         println(Day9.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day9.solve2(example))
         println(Day9.solve2(puzzleInput))
     }
 }
