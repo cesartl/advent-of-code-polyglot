3cc1271288b9b4b8441da158dfa68ecfeaba1bee
Cesar Tron-Lozai <ctron@thousandeyes.com>
2023-12-20 13:26:46 +0100

    2023 day12 p1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day12.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2023/Day12.kt
@@ -1,8 +1,58 @@
 package com.ctl.aoc.kotlin.y2023
 
+data class SpringRecord(
+    val record: String,
+    val groups: List<Int>
+) {
+    val regex: Regex by lazy {
+        groups.joinToString(
+            separator = "\\.+",
+            prefix = "\\.*",
+            postfix = "\\.*",
+        ) {
+            "#{$it}"
+        }.toRegex()
+    }
+
+    fun matches(record: String): Boolean = regex.matches(record)
+
+    fun generateRecords(): Sequence<String> {
+        return go(0, "")
+            .filter { this.matches(it) }
+    }
+
+    private fun go(index: Int, current: String): Sequence<String> = sequence {
+        if (index == record.length) {
+            yield(current)
+        } else {
+            val c = record[index]
+            if (c == '?') {
+                yieldAll(go(index + 1, "$current."))
+                yieldAll(go(index + 1, "$current#"))
+            } else {
+                yieldAll(go(index + 1, current + c))
+            }
+        }
+    }
+}
+
+private fun String.parseRecord(): SpringRecord {
+    val parts = this.split(" ")
+    val record = parts[0]
+    val groups = parts[1]
+        .splitToSequence(",")
+        .map { it.trim() }
+        .map { it.toInt() }
+        .toList()
+    return SpringRecord(record, groups)
+}
+
 object Day12 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        return input
+            .map { it.parseRecord() }
+            .map { it.generateRecords().count() }
+            .sum()
     }
 
     fun solve2(input: Sequence<String>): Int {
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day12Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2023/Day12Test.kt
@@ -7,8 +7,16 @@ internal class Day12Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2023, 12)
 
+    val example = """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day12.solve1(example))
         println(Day12.solve1(puzzleInput))
     }
 
