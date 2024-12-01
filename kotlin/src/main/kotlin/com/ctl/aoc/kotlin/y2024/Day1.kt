15c07c324574fb608871e01a9cba4c5af90bf1f4
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-01 21:18:05 +0000

    2024 day1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day1.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day1.kt
@@ -1,11 +1,33 @@
 package com.ctl.aoc.kotlin.y2024
 
+import com.ctl.aoc.kotlin.utils.frequency
+import kotlin.math.abs
+
 object Day1 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val (left, right) = parseLists(input)
+        return left.asSequence().sorted().zip(right.asSequence().sorted())
+            .map { (l, r) -> abs(l - r) }
+            .sum()
     }
 
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        val (left, right) = parseLists(input)
+        val rightFrequency = right.frequency()
+        return left.asSequence()
+            .map { it * (rightFrequency[it] ?: 0) }
+            .sum()
+    }
+
+    private fun parseLists(input: Sequence<String>): Pair<MutableList<Int>, MutableList<Int>> {
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+        input.forEach { line ->
+            //regex for whitespace
+            val split = line.split("\\s+".toRegex())
+            left.add(split[0].toInt())
+            right.add(split[1].toInt())
+        }
+        return Pair(left, right)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day1Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day1Test.kt
@@ -7,13 +7,22 @@ internal class Day1Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2024, 1)
 
+    val example = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day1.solve1(example))
         println(Day1.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day1.solve2(example))
         println(Day1.solve2(puzzleInput))
     }
 }
