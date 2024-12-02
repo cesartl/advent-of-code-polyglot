2fd114a5802f285277c08f55f224dfc858e799ce
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-02 10:36:34 +0000

    2024 day2

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day2.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day2.kt
@@ -1,11 +1,57 @@
 package com.ctl.aoc.kotlin.y2024
 
+import kotlin.math.sign
+
+data class Report(
+    val levels: List<Int>
+) {
+    fun isSafe(): Boolean {
+        val diffs = levels.zipWithNext { a, b -> a - b }
+        val firstSign = diffs.first().sign
+        return diffs.all { diffCheck(firstSign, it) }
+    }
+
+    fun isSafe2(): Boolean {
+        return removalCandidates().any { it.isSafe() }
+    }
+
+    private fun diffCheck(firstSign: Int, diff: Int): Boolean{
+        return diff.sign == firstSign && diff != 0 && diff in -3..3
+    }
+
+    private fun removalCandidates(): Sequence<Report>{
+        if(isSafe()){
+            return sequenceOf(this)
+        }
+        val report = this
+        return sequence {
+           report.levels.indices.forEach { index ->
+                val newLevels = report.levels.toMutableList()
+                newLevels.removeAt(index)
+                yield(Report(newLevels))
+           }
+        }
+    }
+
+}
+
+private fun parseInput(input: Sequence<String>): List<Report> {
+    return input
+        .map { line ->
+            line.split(" ").map { it.toInt() }
+        }.map { Report(it) }
+        .toList()
+
+}
+
 object Day2 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val reports = parseInput(input)
+        return reports.count { it.isSafe() }
     }
 
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        val reports = parseInput(input)
+        return reports.count { it.isSafe2() }
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day2Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day2Test.kt
@@ -7,13 +7,24 @@ internal class Day2Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2024, 2)
 
+    val example = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day2.solve1(example))
         println(Day2.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day2.solve2(example))
+        //>322
+        //>339
         println(Day2.solve2(puzzleInput))
     }
 }
