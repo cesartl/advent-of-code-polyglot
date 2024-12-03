e23b232aa6639ba6ff92829eb05f8e49c4341cbc
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-03 19:52:47 +0000

    2024 day3

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day3.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day3.kt
@@ -1,11 +1,34 @@
 package com.ctl.aoc.kotlin.y2024
 
 object Day3 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    private val regex = """mul\((\d+),(\d+)\)""".toRegex()
+
+
+    fun solve1(input: String): Long {
+        return regex
+            .findAll(input).map {
+                val (a, b) = it.destructured
+                a.toLong() * b.toLong()
+            }
+            .sum()
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private data class Acc(val enabled: Boolean, val value: Long)
+
+    private val regex2 = """do\(\)|don't\(\)|mul\((\d+),(\d+)\)""".toRegex()
+    fun solve2(input: String): Long {
+        return regex2.findAll(input).fold(Acc(true, 0)) { acc, match ->
+            if (match.value.contains("do()")) {
+                acc.copy(enabled = true)
+            } else if (match.value.contains("don't()")) {
+                acc.copy(enabled = false)
+            } else if (acc.enabled) {
+                val (a, b) = match.destructured
+                acc.copy(value = acc.value + a.toLong() * b.toLong())
+            } else {
+                acc
+            }
+        }.value
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day3Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day3Test.kt
@@ -5,15 +5,19 @@ import org.junit.jupiter.api.Test
 
 internal class Day3Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 3)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 3)
+    val example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    val example2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
     @Test
     fun solve1() {
+        println(Day3.solve1(example))
         println(Day3.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day3.solve2(example2))
         println(Day3.solve2(puzzleInput))
     }
 }
