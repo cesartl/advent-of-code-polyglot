c108a2bb383b1fb7d2baec821f02b6695b330f61
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-05 12:39:19 +0000

    2024 day5

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day5.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day5.kt
@@ -1,11 +1,104 @@
 package com.ctl.aoc.kotlin.y2024
 
+import com.ctl.aoc.kotlin.utils.Graph
+
 object Day5 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    data class SortingRule(val from: Int, val to: Int)
+
+    fun solve1(input: String): Int {
+        val (rules, pages) = parseInput(input)
+        return pages.asSequence()
+            .filter { page ->
+                val sortedByIndex: Map<Int, Int> =
+                    buildOrder(page, rules).withIndex().associate { it.value to it.index }
+                isSorted(sortedByIndex, page)
+            }
+            .map { page ->
+                page[page.size / 2]
+            }
+            .sum()
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun isSorted(sortedByIndex: Map<Int, Int>, page: List<Int>): Boolean {
+        (0 until page.size - 1).forEach { i ->
+            (i + 1 until page.size).forEach { j ->
+                if (getIndex(sortedByIndex, page[i]) > getIndex(sortedByIndex, page[j])) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun buildOrder(page: List<Int>, rules: List<SortingRule>): List<Int> {
+        val graph = Graph<Int>()
+        val rootNodes = ArrayDeque<Int>()
+        val inScope = page.toSet()
+        val sorted = mutableListOf<Int>()
+        rules.forEach { rule ->
+            if (inScope.contains(rule.from) && inScope.contains(rule.to)) {
+                graph.addDirectedEdge(rule.from, rule.to)
+            }
+        }
+
+        page.forEach { node ->
+            if (graph.incomingNodes(node).isEmpty()) {
+                rootNodes.add(node)
+            }
+        }
+
+        while (rootNodes.isNotEmpty()) {
+            val current = rootNodes.removeFirst()
+            sorted.add(current)
+            graph.outgoingNodes(current).forEach { to ->
+                graph.removeEdge(current, to)
+                if (graph.incomingNodes(to).isEmpty()) {
+                    rootNodes.add(to)
+                }
+            }
+        }
+
+        return sorted
+    }
+
+    private fun getIndex(sortedByIndex: Map<Int, Int>, n: Int): Int {
+        return sortedByIndex[n] ?: error("Missing $n")
+    }
+
+    fun solve2(input: String): Int {
+        val (rules, pages) = parseInput(input)
+        return pages.asSequence()
+            .mapNotNull { page ->
+                val sortedByIndex: Map<Int, Int> =
+                    buildOrder(page, rules).withIndex().associate { it.value to it.index }
+                if (isSorted(sortedByIndex, page)) {
+                    null
+                } else {
+                    page.sortedWith { a, b ->
+                        val ai = getIndex(sortedByIndex, a)
+                        val bi = getIndex(sortedByIndex, b)
+                        ai.compareTo(bi)
+                    }
+                }
+            }
+            .map { page ->
+                page[page.size / 2]
+            }
+            .sum()
+    }
+
+    data class PrintingInput(val rules: List<SortingRule>, val pages: List<List<Int>>)
+
+    private fun parseInput(input: String): PrintingInput {
+        val parts = input.trim().split("\n\n")
+        val rules = parts[0].splitToSequence("\n").map { line ->
+            val (from, to) = line.split("|").map { it.toInt() }
+            SortingRule(from, to)
+        }.toList()
+        val pages = parts[1].splitToSequence("\n").map { line ->
+            line.splitToSequence(",").map { it.toInt() }.toList()
+        }.toList()
+        return PrintingInput(rules, pages)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day5Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day5Test.kt
@@ -5,15 +5,47 @@ import org.junit.jupiter.api.Test
 
 internal class Day5Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 5)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 5)
+
+    val example = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"""
 
     @Test
     fun solve1() {
+        println(Day5.solve1(example))
+        //<10604
         println(Day5.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day5.solve2(example))
         println(Day5.solve2(puzzleInput))
     }
 }
