83d5eed276528767b05749acde0aee903b0c0527
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-07 11:51:48 +0000

    2024 day7

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day7.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day7.kt
@@ -1,11 +1,102 @@
 package com.ctl.aoc.kotlin.y2024
 
+
 object Day7 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    sealed class Operator {
+        abstract fun apply(x: Long, y: Long): Long
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    data object Add : Operator() {
+        override fun apply(x: Long, y: Long): Long = x + y
+    }
+
+    data object Mul : Operator() {
+        override fun apply(x: Long, y: Long): Long = x * y
+    }
+
+    data object Concat : Operator() {
+        override fun apply(x: Long, y: Long): Long {
+            return (x.toString() + y.toString()).toLong()
+        }
+    }
+
+    data class State(val result: Long, val operators: List<Operator>)
+
+    private val operatorsCache: MutableMap<Int, List<List<Operator>>> = mutableMapOf()
+
+    data class Equation(val result: Long, val numbers: List<Long>) {
+
+        fun isPossible(): Boolean {
+            val n = numbers.size - 1
+            val operators = operatorsCache.computeIfAbsent(n) {
+                generateOperators(
+                    numbers.size - 1,
+                    listOf(Add, Mul),
+                    listOf(emptyList())
+                )
+            }
+            return operators.any { isMatch(it) }
+        }
+
+        fun isPossible2(): Boolean {
+            val n = numbers.size - 1
+            val operators = operatorsCache.computeIfAbsent(n) {
+                generateOperators(
+                    numbers.size - 1,
+                    listOf(Add, Mul, Concat),
+                    listOf(emptyList())
+                )
+            }
+            return operators.any { isMatch(it) }
+        }
+
+        private fun isMatch(operators: List<Operator>): Boolean {
+            val first = numbers.first()
+            val result = numbers.asSequence()
+                .drop(1)
+                .fold(State(first, operators)) { state, current ->
+                    val result = state.operators.first().apply(state.result, current)
+                    State(result, state.operators.drop(1))
+                }
+            return result.result == this.result
+        }
+    }
+
+    fun solve1(input: Sequence<String>): Long {
+        return input
+            .map { parseEquation(it) }
+            .filter { it.isPossible() }
+            .map { it.result }
+            .sum()
+    }
+
+    fun solve2(input: Sequence<String>): Long {
+        return input
+            .map { parseEquation(it) }
+            .filter { it.isPossible2() }
+            .map { it.result }
+            .sum()
+    }
+
+    private fun parseEquation(line: String): Equation {
+        val (result, line) = line.split(":")
+        val x = line.trim().splitToSequence(" ").map { it.toLong() }.toList()
+        return Equation(result.trim().toLong(), x)
+    }
+
+    private tailrec fun <T> generateOperators(
+        n: Int,
+        operators: List<T>,
+        acc: List<List<T>>
+    ): List<List<T>> {
+        if (n == 0) {
+            return acc
+        }
+        val newAcc = acc.flatMap { line ->
+            operators.map { operator -> line + operator }
+        }
+        return generateOperators(n - 1, operators, newAcc)
     }
 }
+
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day7Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day7Test.kt
@@ -7,13 +7,25 @@ internal class Day7Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2024, 7)
 
+    val example = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day7.solve1(example))
         println(Day7.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day7.solve2(example))
         println(Day7.solve2(puzzleInput))
     }
 }
