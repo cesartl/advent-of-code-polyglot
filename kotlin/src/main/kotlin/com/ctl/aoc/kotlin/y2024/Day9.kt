8bfc67e8a585b5b3bea7e1422348288166bd6c43
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-10 15:18:48 +0000

    2024 day9

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day9.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day9.kt
@@ -1,11 +1,115 @@
 package com.ctl.aoc.kotlin.y2024
 
+import java.math.BigInteger
+
 object Day9 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: String): Long {
+        val files = input
+            .trim()
+            .map { it.digitToInt() }
+        val size = files.sum()
+        val blocks = IntArray(size) { -1 }
+
+        var isFile = true
+        var current = 0
+        files.forEachIndexed { index, n ->
+            if (isFile) {
+                val id = index / 2
+                repeat(n) {
+                    blocks[current] = id
+                    current++
+                }
+                isFile = false
+            } else {
+                current += n
+                isFile = true
+            }
+        }
+
+        var left = 0
+        var right = size - 1
+
+        while (left < right) {
+            while (blocks[left] != -1) {
+                left++
+            }
+            blocks[left] = blocks[right]
+            blocks[right] = -1
+            right--
+            while (right >= 0 && blocks[right] == -1) {
+                right--
+            }
+            left++
+        }
+
+        val last = blocks.indexOf(-1)
+        return blocks.slice(0 until last)
+            .foldIndexed(0L) { index, acc, i ->
+                acc + index * i.toLong()
+            }
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    data class FileSpec(val id: Int, val index: Int, val size: Int)
+
+    class FreeSpace(var index: Int, var size: Int) {
+        override fun toString(): String {
+            return "$index -> ${index + size}"
+        }
     }
+
+    fun solve2(input: String): String {
+        val files = input
+            .trim()
+            .map { it.digitToInt() }
+        val size = files.sum()
+        val blocks = IntArray(size) { -1 }
+
+        var isFile = true
+        var current = 0
+        val fileSpecs = mutableListOf<FileSpec>()
+        val freeSpaces = mutableListOf<FreeSpace>()
+        files.forEachIndexed { index, n ->
+            if (isFile) {
+                val id = index / 2
+                fileSpecs.add(FileSpec(id, current, n))
+                repeat(n) {
+                    blocks[current] = id
+                    current++
+                }
+                isFile = false
+            } else {
+                freeSpaces.add(FreeSpace(current, n))
+                current += n
+                isFile = true
+            }
+        }
+
+        fileSpecs.reversed().forEach { (id, position, size) ->
+            freeSpaces.withIndex().firstOrNull { (_, freeSpace) ->
+                freeSpace.index < position && freeSpace.size >= size
+            }?.let { (f, freeSpace) ->
+                repeat(size) { i ->
+                    blocks[freeSpace.index + i] = id
+                    blocks[position + i] = -1
+                }
+                freeSpace.index += size
+                freeSpace.size -= size
+                if (freeSpace.size == 0) {
+                    freeSpaces.removeAt(f)
+                }
+            }
+        }
+
+        return blocks
+            .foldIndexed(BigInteger.ZERO) { index, acc, i ->
+                if (i == -1) {
+                    acc
+                } else {
+                    acc + index.toBigInteger() * i.toBigInteger()
+                }
+            }
+            .toString()
+    }
+
+
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day9Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day9Test.kt
@@ -5,15 +5,21 @@ import org.junit.jupiter.api.Test
 
 internal class Day9Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 9)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 9)
+
+    val example = "2333133121414131402"
 
     @Test
     fun solve1() {
-        println(Day9.solve1(puzzleInput))
+        println(Day9.solve1(example))
+//        println(Day9.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        println(Day9.solve2(puzzleInput))
+        println(Day9.solve2(example))
+//        println(Day9.solve2("12345"))
+        //6321896265143
+        println (Day9.solve2(puzzleInput))
     }
 }
