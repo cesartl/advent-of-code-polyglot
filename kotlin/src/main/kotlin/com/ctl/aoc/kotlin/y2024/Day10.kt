7919e4e32400f566c19d81f9ee768e890708d1ca
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-10 15:18:54 +0000

    2024 day10

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day10.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day10.kt
@@ -1,11 +1,59 @@
 package com.ctl.aoc.kotlin.y2024
 
+import com.ctl.aoc.kotlin.utils.*
+
 object Day10 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val grid = parseGrid(input) { it.digitToInt() }
+        return grid.map.asSequence()
+            .filter { it.value == 0 }
+            .map { entry ->
+                val start = entry.key
+                val result = path(start, grid)
+                val c = result.steps.count { grid.map[it.key] == 9 }
+                println("${entry.key} -> $c")
+                c
+            }
+            .sum()
+    }
+
+    private fun path(start: Position, grid: Grid<Int>): PathingResult<Position> {
+        return Dijkstra.traverse(
+            start = start,
+            nodeGenerator = { pos ->
+                val currentHeight = grid.map[pos] ?: 0
+                pos.adjacent()
+                    .filter { grid.inScope(it) && grid.map[it] == currentHeight + 1 }
+            },
+            distance = { _, _ -> 1 },
+            end = null
+        )
+    }
+
+    private fun path2(start: Position, grid: Grid<Int>): Sequence<List<Position>> {
+        return traversal(
+            startNode = listOf(start),
+            storage = Stack(),
+            nodeGenerator = { poss ->
+                val pos = poss.last()
+                val currentHeight = grid.map[pos] ?: 0
+                pos.adjacent()
+                    .filter { grid.inScope(it) && grid.map[it] == currentHeight + 1 }
+                    .map { poss + it }
+            },
+            index = { it.toString() }
+        )
     }
 
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        val grid = parseGrid(input) { it.digitToInt() }
+        return grid.map.asSequence()
+            .filter { it.value == 0 }
+            .map { entry ->
+                val start = entry.key
+                val paths = path2(start, grid)
+                paths.count { grid.map[it.last()] == 9 }
+            }
+            .sum()
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day10Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day10Test.kt
@@ -7,13 +7,24 @@ internal class Day10Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2024, 10)
 
+    val example = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732""".splitToSequence("\n")
+
     @Test
     fun solve1() {
+        println(Day10.solve1(example))
         println(Day10.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day10.solve2(example))
         println(Day10.solve2(puzzleInput))
     }
 }
