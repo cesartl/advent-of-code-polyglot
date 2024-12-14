6b50304aa29d6e07c76731a746517d3299f15e5c
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-14 13:43:15 +0000

    2024 day11

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day11.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day11.kt
@@ -1,11 +1,46 @@
 package com.ctl.aoc.kotlin.y2024
 
 object Day11 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: Sequence<String>): Long {
+        return blink(input, 25)
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun blink(input: Sequence<String>, n: Int): Long {
+        return input.map { countStones(it, n) }.sum()
+    }
+
+    private val cache = mutableMapOf<Pair<String, Int>, Long>()
+
+    private fun countStones(stone: String, remaining: Int): Long{
+        val cached = cache[Pair(stone, remaining)]
+        if(cached != null){
+            return cached
+        }
+        if(remaining == 0 ){
+            return 1
+        }
+        if(stone == "0"){
+            return countStones("1", remaining - 1).let {
+                cache[Pair(stone, remaining)] = it
+                it
+            }
+        }
+        if(stone.length % 2 == 0){
+            val firstHalf = stone.substring(0, stone.length / 2).toLong().toString()
+            val secondHalf = stone.substring(stone.length / 2).toLong().toString()
+            return (countStones(firstHalf, remaining -1) + countStones(secondHalf, remaining - 1)).let {
+                cache[Pair(stone, remaining)] = it
+                it
+            }
+        }
+        val stoneValue = stone.toLong()
+        return countStones((stoneValue * 2024).toString(), remaining - 1).let {
+            cache[Pair(stone, remaining)] = it
+            it
+        }
+    }
+
+    fun solve2(input: Sequence<String>): Long {
+        return blink(input, 75)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day11Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day11Test.kt
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.Test
 
 internal class Day11Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 11)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 11).trim().splitToSequence(" ")
+    val example = "125 17".splitToSequence(" ")
 
     @Test
     fun solve1() {
+        println(Day11.solve1(example))
         println(Day11.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day11.solve2(example))
         println(Day11.solve2(puzzleInput))
     }
 }
