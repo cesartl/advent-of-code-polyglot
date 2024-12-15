05b4a850ea101f24a3169faa5e7b44f832ec4386
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-15 16:30:16 +0000

    2024 day13

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day13.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day13.kt
@@ -1,11 +1,73 @@
 package com.ctl.aoc.kotlin.y2024
 
+import com.ctl.aoc.kotlin.y2023.LongPosition
+
 object Day13 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    data class ClawMachine(
+        val a: LongPosition,
+        val b: LongPosition,
+        val prize: LongPosition
+    )
+
+    fun solve1(input: String): Long {
+        val machines = parse(input)
+        return machines.mapNotNull { winPrice(it) }.sum()
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+
+    private fun winPrice(machine: ClawMachine): Long? {
+        val (a, b, prize) = machine
+        val (X, Y) = prize
+        val bottom = b.y * a.x - b.x * a.y
+        if (bottom == 0L) {
+            return null
+        }
+        val top = Y * a.x - X * a.y
+        if (top % bottom != 0L) {
+            return null
+        }
+        val B = top / bottom
+
+
+        val a1 = X - B * b.x
+        if(a1 % a.x != 0L){
+            return null
+        }
+        val A = a1 / a.x
+        return 3 * A + B
+    }
+
+    fun solve2(input: String): Long {
+        val machines = parse(input)
+        val extra = 10000000000000L
+        val results = machines
+            .map { it.copy(prize = LongPosition(it.prize.x + extra, it.prize.y + extra, 0)) }
+            .map { winPrice(it) }
+            .toList()
+        return results.asSequence().filterNotNull().sum()
+    }
+
+    private val regex = """X([+\-0-9]+), Y([+\-0-9]+)""".toRegex()
+    private val regex2 = """X=([-0-9]+), Y=([+0-9]+)""".toRegex()
+
+    private fun parsePosition(input: String): LongPosition {
+        val (x, y) = (regex.matchEntire(input) ?: error("Not valid: $input")).destructured
+        return LongPosition(x.toLong(), y.toLong(), 0L)
+    }
+
+    private fun parsePrize(input: String): LongPosition {
+        val (x, y) = (regex2.matchEntire(input) ?: error("Not valid: $input")).destructured
+        return LongPosition(x.toLong(), y.toLong(), 0L)
+    }
+
+    private fun parse(input: String): Sequence<ClawMachine> {
+        return input.splitToSequence("\n\n").map { block ->
+            val lines = block.trim().lines()
+            val a = parsePosition(lines[0].substringAfter("Button A: "))
+            val b = parsePosition(lines[1].substringAfter("Button B: "))
+            val prize = parsePrize(lines[2].substringAfter("Prize: "))
+            ClawMachine(a, b, prize)
+        }
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day13Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day13Test.kt
@@ -5,15 +5,36 @@ import org.junit.jupiter.api.Test
 
 internal class Day13Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 13)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 13)
+
+    val example = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
 
     @Test
     fun solve1() {
+        println(Day13.solve1(example))
+        //>22427
         println(Day13.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day13.solve2(example))
+        //<98958951401149
+        // 98080815200063
         println(Day13.solve2(puzzleInput))
     }
 }
