39139d9580fe9ab8f9bbaaffc8b3cc02fb5f5364
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-17 10:22:33 +0000

    2024 day15 part 1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day15.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day15.kt
@@ -1,11 +1,180 @@
 package com.ctl.aoc.kotlin.y2024
 
+import com.ctl.aoc.kotlin.utils.*
+
 object Day15 {
-    fun solve1(input: Sequence<String>): Int {
+
+    data class Warehouse(
+        val walls: Set<Position>,
+        val boxes: Set<Position>,
+        val robot: Position,
+        val xRange: IntRange,
+        val yRange: IntRange
+    ) {
+
+        val gpsValue: Int by lazy {
+            boxes.sumOf { (x, y) -> x + 100 * y }
+        }
+
+        fun moveRobot(instruction: Char): Warehouse {
+            val direction = parseInstruction(instruction)
+            val nextRobot = direction.move(robot)
+
+            if (walls.contains(nextRobot)) {
+                return this
+            }
+
+            if (!boxes.contains(nextRobot)) {
+                return this.copy(robot = nextRobot)
+            }
+
+            val boxesToMove = generateSequence(nextRobot) { direction.move(it) }
+                .takeWhile { boxes.contains(it) }
+                .toList()
+
+            val emptySlot = direction.move(boxesToMove.last())
+
+            if (walls.contains(emptySlot)) {
+                return this
+            }
+
+            val newBoxes = boxes.toMutableSet()
+            newBoxes.removeAll(boxesToMove.toSet())
+            newBoxes.addAll(boxesToMove.map { direction.move(it) })
+            return this.copy(boxes = newBoxes, robot = nextRobot)
+        }
+
+        fun print() {
+            yRange.forEach { y ->
+                xRange.forEach { x ->
+                    val p = Position(x, y)
+                    when {
+                        walls.contains(p) -> print('#')
+                        boxes.contains(p) -> print('O')
+                        robot == p -> print('@')
+                        else -> print('.')
+                    }
+                }
+                println()
+            }
+        }
+    }
+
+    fun solve1(input: String): Int {
+        val (warehouse, instructions) = parseWarehouse(input)
+
+        val result = instructions.fold(warehouse) { w, i ->
+            w.moveRobot(i)
+        }
+
+        return result.gpsValue
+    }
+
+    fun solve2(input: String): Int {
+        val (largeWarehouse, instructions) = parseLargeWarehouse(input)
+        largeWarehouse.print()
         TODO()
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    data class LargeBox(val left: Position, val right: Position)
+
+    data class LargeWarehouse(
+        val walls: Set<Position>,
+        val boxes: Set<LargeBox>,
+        val robot: Position,
+        val xRange: IntRange,
+        val yRange: IntRange
+    ) {
+
+        private val boxPositions = boxes.flatMap { listOf(it.left, it.right) }.toSet()
+
+        fun moveRobot(instruction: Char): LargeWarehouse {
+            TODO()
+        }
+
+        fun print() {
+            yRange.forEach { y ->
+                var x = 0
+                while(x in xRange){
+                    val p = Position(x, y)
+                    when {
+                        walls.contains(p) -> print('#')
+                        boxPositions.contains(p) -> {
+                            print("[]")
+                            x++
+                        }
+
+                        robot == p -> print('@')
+                        else -> print('.')
+                    }
+                    x++
+                }
+                println()
+            }
+        }
+    }
+
+    private fun parseWarehouse(input: String): Pair<Warehouse, String> {
+        val (gridString, instructions) = input.split("\n\n")
+        val grid = parseGrid(gridString.trim().lineSequence())
+
+        val walls = grid.map.asSequence()
+            .filter { it.value == '#' }
+            .map { it.key }
+            .toSet()
+
+        val boxes = grid.map.asSequence()
+            .filter { it.value == 'O' }
+            .map { it.key }
+            .toSet()
+
+        val robot = grid.map.asSequence()
+            .filter { it.value == '@' }
+            .map { it.key }
+            .single()
+
+        return Warehouse(walls, boxes, robot, grid.xRange, grid.yRange) to instructions.trim().replace("\n", "")
+    }
+
+    private fun parseLargeWarehouse(input: String): Pair<LargeWarehouse, String> {
+        val (gridString, instructions) = input.split("\n\n")
+        val grid = parseGrid(gridString.trim().lineSequence().map { it.enlarge() })
+
+        val walls = grid.map.asSequence()
+            .filter { it.value == '#' }
+            .map { it.key }
+            .toSet()
+
+        val boxes = grid.map.asSequence()
+            .filter { it.value == '[' }
+            .map { it.key }
+            .map { LargeBox(it, it.copy(x = it.x + 1)) }
+            .toSet()
+
+        val robot = grid.map.asSequence()
+            .filter { it.value == '@' }
+            .map { it.key }
+            .single()
+
+
+        return LargeWarehouse(walls, boxes, robot, grid.xRange, grid.yRange) to instructions.trim().replace("\n", "")
+    }
+
+}
+
+private fun String.enlarge(): String {
+    return this.replace("#", "##")
+        .replace("O", "[]")
+        .replace(".", "..")
+        .replace("@", "@.")
+}
+
+private fun parseInstruction(instruction: Char): Orientation {
+    return when (instruction) {
+        '^' -> N
+        'v' -> S
+        '<' -> W
+        '>' -> E
+        else -> throw IllegalArgumentException("Invalid instruction '$instruction'")
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day15Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day15Test.kt
@@ -5,15 +5,50 @@ import org.junit.jupiter.api.Test
 
 internal class Day15Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 15)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 15)
+
+    val example1 = """########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<"""
+
+    val example2 = """##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
 
     @Test
     fun solve1() {
+        println(Day15.solve1(example1))
+        println(Day15.solve1(example2))
         println(Day15.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
-        println(Day15.solve2(puzzleInput))
+        println(Day15.solve2(example2))
     }
 }
