b3537510edebfbabbac25c079ded79954fa1cb06
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-19 09:04:58 +0000

    2024 day19

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day19.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day19.kt
@@ -1,11 +1,48 @@
 package com.ctl.aoc.kotlin.y2024
 
 object Day19 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    data class TowelSpec(
+        val patterns: List<String>,
+        val designs: List<String>
+    ) {
+        val regex = patterns.joinToString(separator = "|", prefix = "(", postfix = ")+")
+            .toRegex()
+
+        private val cache = mutableMapOf<String, Long>()
+
+        fun countWays(design: String): Long {
+            val cached = cache[design]
+            if (cached != null) {
+                return cached
+            }
+            if (design.isEmpty()) {
+                return 1
+            }
+            val count = patterns.asSequence()
+                .filter { pattern -> design.startsWith(pattern) }
+                .map { pattern -> design.removePrefix(pattern) }
+                .map { pattern -> countWays(pattern) }
+                .sum()
+            cache[design] = count
+            return count
+        }
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: String): Int {
+        val spec = input.parseTowelSpec()
+        return spec.designs.count { spec.regex.matches(it) }
     }
+
+    fun solve2(input: String): Long {
+        val spec = input.parseTowelSpec()
+        return spec.designs.sumOf { spec.countWays(it) }
+    }
+}
+
+private fun String.parseTowelSpec(): Day19.TowelSpec {
+    val (top, bottom) = this.split("\n\n")
+    val patterns = top.trim().splitToSequence(",").map { it.trim() }.toList()
+    val designs = bottom.trim().lines()
+    return Day19.TowelSpec(patterns, designs)
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day19Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day19Test.kt
@@ -5,15 +5,29 @@ import org.junit.jupiter.api.Test
 
 internal class Day19Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 19)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 19)
+
+    val example = """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
+"""
 
     @Test
     fun solve1() {
+        println(Day19.solve1(example))
         println(Day19.solve1(puzzleInput))
     }
 
     @Test
     fun solve2() {
+        println(Day19.solve2(example))
         println(Day19.solve2(puzzleInput))
     }
 }
