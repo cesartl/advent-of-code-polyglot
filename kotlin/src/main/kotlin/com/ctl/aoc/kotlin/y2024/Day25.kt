9c88f9cf3f60a0736cbcf975d82a8cb0c90803d2
Cesar Tron-Lozai <ctron@thousandeyes.com>
2024-12-28 09:57:09 +0000

    2024 day25

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day25.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2024/Day25.kt
@@ -1,11 +1,56 @@
 package com.ctl.aoc.kotlin.y2024
 
+import com.ctl.aoc.kotlin.utils.parseGrid
+
 object Day25 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: String): Int {
+        val locks = mutableListOf<List<Int>>()
+        val keys = mutableListOf<List<Int>>()
+        input.splitToSequence("\n\n")
+            .map { it.trim() }
+            .forEach {
+                if (it.startsWith("#")) {
+                    locks.add(parseLock(it))
+                } else {
+                    keys.add(parseKey(it))
+                }
+            }
+        var count = 0
+        locks.forEach { lock ->
+            keys.forEach { key ->
+                if (lock.zip(key)
+                        .asSequence()
+                        .map { (a, b) -> a + b }
+                        .all { it < 6 }
+                ) {
+                    count++
+                }
+            }
+        }
+        return count
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    private fun parseLock(string: String): List<Int> {
+        val grid = parseGrid(string.lineSequence()) {
+            if (it == '#') it else null
+        }
+        return grid.xRange.map { x ->
+            grid.map.entries
+                .asSequence()
+                .filter { it.key.x == x }
+                .maxOf { it.key.y }
+        }
+    }
+
+    private fun parseKey(string: String): List<Int> {
+        val grid = parseGrid(string.lineSequence()) {
+            if (it == '#') it else null
+        }
+        return grid.xRange.map { x ->
+            grid.map.entries
+                .asSequence()
+                .filter { it.key.x == x }
+                .minOf { it.key.y }
+        }.map { 6 - it }
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day25Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2024/Day25Test.kt
@@ -5,15 +5,52 @@ import org.junit.jupiter.api.Test
 
 internal class Day25Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2024, 25)
+    val puzzleInput = InputUtils.downloadAndGetString(2024, 25)
+
+    val example = """#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####"""
 
     @Test
     fun solve1() {
+        println(Day25.solve1(example))
         println(Day25.solve1(puzzleInput))
     }
 
-    @Test
-    fun solve2() {
-        println(Day25.solve2(puzzleInput))
-    }
 }
