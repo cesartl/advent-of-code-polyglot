434f8c6731984f349a3cab52984a9b0437ea6c1e
Cesar Tron-Lozai <ctron@thousandeyes.com>
2025-12-01 07:52:56 +0000

    2025 day1

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day1.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day1.kt
@@ -2,10 +2,72 @@ package com.ctl.aoc.kotlin.y2025
 
 object Day1 {
     fun solve1(input: Sequence<String>): Int {
-        TODO()
+        val n = 100
+        val positions = input.runningFold(50) { dialPosition, rotation ->
+            val sign = if (rotation.startsWith("L")) {
+                -1
+            } else {
+                1
+            }
+            val count = rotation.drop(1).toInt() % n
+            (dialPosition + sign * count + n) % n
+        }
+        return positions.count { it == 0 }
     }
 
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        var current = 50
+        val n = 100
+        val positions = sequence {
+            input.forEach { rotation ->
+                val sign = when (val direction = rotation[0]) {
+                    'L' -> -1
+                    'R' -> 1
+                    else -> error("Unknown direction $direction")
+                }
+                val count = rotation.drop(1).toInt()
+                repeat(count) {
+                    current = (current + sign + n) % n
+                    yield(current)
+                }
+            }
+        }
+        return positions.count { it == 0 }
+    }
+
+    fun solve2Bis(input: Sequence<String>): Int {
+        val n = 100
+        val (_, zeroCount) = input.fold(50 to 0) { (dialPosition, zeroCount), rotation ->
+            val direction = rotation[0]
+            val sign = when (direction) {
+                'L' -> -1
+                'R' -> 1
+                else -> error("Unknown direction $direction")
+            }
+            val count = rotation.drop(1).toInt()
+            val k0 = when (direction) {
+                'R' -> {
+                    (n - dialPosition) % n
+                }
+
+                'L' -> {
+                    dialPosition % n
+                }
+
+                else -> error("Unknown direction $direction")
+            }
+
+            val newZeroCount = if (dialPosition == 0) {
+                count / n
+            } else if (count < k0) {
+                0
+            } else {
+                1 + (count - k0) / n
+            }
+
+            val newPosition = dialPosition + sign * (count % n)
+            (newPosition + n) % n to zeroCount + newZeroCount
+        }
+        return zeroCount
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day1Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day1Test.kt
@@ -1,6 +1,8 @@
 package com.ctl.aoc.kotlin.y2025
 
 import com.ctl.aoc.kotlin.utils.InputUtils
+import com.ctl.aoc.kotlin.utils.timed
+import com.ctl.aoc.kotlin.utils.timedMs
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -9,7 +11,16 @@ internal class Day1Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2025, 1)
 
-    val example = """foo""".lineSequence()
+    val example = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82""".lineSequence()
 
     @Test
     fun solve1() {
@@ -19,7 +30,10 @@ internal class Day1Test {
 
     @Test
     fun solve2() {
-        println(Day1.solve2(example))
-        println(Day1.solve2(puzzleInput))
+        println(timedMs { Day1.solve2(example) })
+        println(timedMs { Day1.solve2(puzzleInput) })
+
+        println(timedMs { Day1.solve2Bis(example) })
+        println(timedMs { Day1.solve2Bis(puzzleInput) })
     }
 }
