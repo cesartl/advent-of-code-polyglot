1a86ffffa2bda6572ef669d77d31be9874eedb46
Cesar Tron-Lozai <ctron@thousandeyes.com>
2025-12-03 07:28:09 +0000

    2025 day2

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day2.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day2.kt
@@ -1,11 +1,32 @@
 package com.ctl.aoc.kotlin.y2025
 
 object Day2 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+
+    private val invalidRegex1 = Regex("^(\\d+)\\1$")
+    private val invalidRegex2 = Regex("^(\\d+)\\1+$")
+
+    fun solve1(input: String): Long {
+        return findInvalidIds(invalidRegex1,input)
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    fun solve2(input: String): Long {
+        return findInvalidIds(invalidRegex2,input)
+    }
+
+    private fun findInvalidIds(regex: Regex, input: String): Long {
+        return input.splitToSequence(",")
+            .flatMap {
+                val (start, end) = it.split("-")
+                val range = LongRange(start.toLong(), end.toLong())
+                findInvalidIds(regex, range)
+            }
+            .sum()
+    }
+
+    private fun findInvalidIds(regex: Regex, range: LongRange): Sequence<Long>{
+        return range.asSequence()
+            .filter {
+                regex.matches(it.toString())
+            }
     }
 }
