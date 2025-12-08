4e4d50fa3b1f6ab9d41008ac5b4dba71e60f853d
Cesar Tron-Lozai <ctron@thousandeyes.com>
2025-12-08 07:11:05 +0000

    2025 day8

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day8.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day8.kt
@@ -1,11 +1,61 @@
 package com.ctl.aoc.kotlin.y2025
 
+import com.ctl.aoc.kotlin.utils.Position3d
+import com.ctl.aoc.kotlin.utils.pairs
+
 object Day8 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: Sequence<String>, n: Int = 10): Int {
+        val positions = parsePositions(input)
+        val circuits: MutableMap<Position3d, Set<Position3d>> = positions.associateWithTo(mutableMapOf()) { setOf(it) }
+        val sortedPairs = positions.pairs()
+            .map { it to distance(it.first, it.second) }
+            .sortedBy { it.second }
+
+        sortedPairs.take(n).forEach { (p, _) ->
+            val (p1, p2) = p
+            val newCircuit = circuits[p1]!! + circuits[p2]!!
+            newCircuit.forEach { p ->
+                circuits[p] = newCircuit
+            }
+        }
+
+        val allCircuits = circuits.values.distinct()
+        return allCircuits
+            .asSequence()
+            .sortedByDescending { it.size }
+            .take(3)
+            .fold(1) { acc, i -> acc * i.size }
+    }
+
+    fun distance(first: Position3d, second: Position3d): Long {
+        val (x1, y1, z1) = first
+        val (x2, y2, z2) = second
+        return (x1 - x2) * (x1 - x2).toLong() + (y1 - y2) * (y1 - y2).toLong() + (z1 - z2) * (z1 - z2).toLong()
     }
 
     fun solve2(input: Sequence<String>): Int {
-        TODO()
+        val positions = parsePositions(input)
+        val circuits: MutableMap<Position3d, Set<Position3d>> = positions.associateWithTo(mutableMapOf()) { setOf(it) }
+        val sortedPairs = positions.pairs()
+            .map { it to distance(it.first, it.second) }
+            .sortedBy { it.second }
+
+        var toConnect: Pair<Position3d, Position3d>
+        val iterator = sortedPairs.iterator()
+        do {
+            toConnect = iterator.next().first
+            val (p1, p2) = toConnect
+            val newCircuit = circuits[p1]!! + circuits[p2]!!
+            newCircuit.forEach { p ->
+                circuits[p] = newCircuit
+            }
+        } while (circuits.values.any { it.size != positions.size })
+        val (a, b) = toConnect
+        return a.x * b.x
     }
+
+    private fun parsePositions(input: Sequence<String>): List<Position3d> = input.map {
+        val (x, y, z) = it.split(",").map { t -> t.toInt() }
+        Position3d(x, y, z)
+    }.toList()
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day8Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day8Test.kt
@@ -9,12 +9,31 @@ internal class Day8Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2025, 8)
 
-    val example = """foo""".lineSequence()
+    val example = """162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689""".lineSequence()
 
     @Test
     fun solve1() {
         println(Day8.solve1(example))
-        println(Day8.solve1(puzzleInput))
+        println(Day8.solve1(puzzleInput, 1000))
     }
 
     @Test
