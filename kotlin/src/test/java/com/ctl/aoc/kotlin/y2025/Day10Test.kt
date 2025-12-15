cdb58e0c56b0d207f310cde41207eab72ba26474
Cesar Tron-Lozai <ctron@thousandeyes.com>
2025-12-15 09:53:44 +0000

    2025 day10

--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day10Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day10Test.kt
@@ -9,7 +9,9 @@ internal class Day10Test {
 
     val puzzleInput = InputUtils.downloadAndGetLines(2025, 10)
 
-    val example = """foo""".lineSequence()
+    val example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}""".lineSequence()
 
     @Test
     fun solve1() {
@@ -21,5 +23,11 @@ internal class Day10Test {
     fun solve2() {
         println(Day10.solve2(example))
         println(Day10.solve2(puzzleInput))
+    }
+
+    @Test
+    fun solve2Bis() {
+        println(Day10.solve2Bis(example))
+        println(Day10.solve2Bis(puzzleInput))
     }
 }
