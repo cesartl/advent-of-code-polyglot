438f0a6eff4a0c24760b2374a0ea9a0569f29868
Cesar Tron-Lozai <ctron@thousandeyes.com>
2025-12-15 09:54:09 +0000

    2025 day11

--- kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day12.kt
+++ kotlin/src/main/kotlin/com/ctl/aoc/kotlin/y2025/Day12.kt
@@ -1,11 +1,59 @@
 package com.ctl.aoc.kotlin.y2025
 
+import com.ctl.aoc.kotlin.utils.Grid
+import com.ctl.aoc.kotlin.utils.parseGrid
+
 object Day12 {
-    fun solve1(input: Sequence<String>): Int {
-        TODO()
+    fun solve1(input: String): Int {
+        val split = input.split("\n\n").map { it.trim() }
+        val shapes: Map<Int, Shape> = split.asSequence().take(6).map { parseShape(it) }.associateBy { it.id }
+        return split.drop(6).first().lineSequence()
+            .map { parseRegion(it) }
+            .count { region ->
+                val total = region.shapeCount.asSequence()
+                    .sumOf { (shapeId, count) ->
+                        val shape = shapes[shapeId] ?: error("Shape $shapeId not found")
+                        shape.numberOfElements * count
+                    }
+                region.size >= total
+            }
+
     }
 
-    fun solve2(input: Sequence<String>): Int {
-        TODO()
+    data class Shape(
+        val id: Int,
+        val grid: Grid<Char>
+    ){
+        val numberOfElements: Int = grid.map.size
+    }
+
+    private fun parseShape(str: String): Shape {
+        val lines = str.lineSequence()
+        val id = lines.first().first().digitToInt()
+        val grid = parseGrid(lines.drop(1)) {
+            it.takeIf { it == '#' }
+        }
+        return Shape(id, grid)
+    }
+
+    data class Region(
+        val width: Int,
+        val length: Int,
+        val shapeCount: Map<Int, Int>
+    ){
+        val size = width * length
+    }
+
+    private val regionRegex = """(\d+)x(\d+):(.*)""".toRegex()
+
+    private fun parseRegion(str: String): Region {
+        val match = regionRegex.matchEntire(str) ?: error("Invalid input: $str")
+        val width = match.groupValues[1].toInt()
+        val length = match.groupValues[2].toInt()
+        val shapeCount = match.groupValues[3].trim()
+            .splitToSequence(' ')
+            .withIndex()
+            .associate { it.index to it.value.toInt() }
+        return Region(width, length, shapeCount)
     }
 }
--- kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day12Test.kt
+++ kotlin/src/test/java/com/ctl/aoc/kotlin/y2025/Day12Test.kt
@@ -7,19 +7,45 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class Day12Test {
 
-    val puzzleInput = InputUtils.downloadAndGetLines(2025, 12)
+    val puzzleInput = InputUtils.downloadAndGetString(2025, 12)
 
-    val example = """foo""".lineSequence()
+    val example = """0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2"""
 
     @Test
     fun solve1() {
-        println(Day12.solve1(example))
+//        println(Day12.solve1(example))
         println(Day12.solve1(puzzleInput))
-    }
-
-    @Test
-    fun solve2() {
-        println(Day12.solve2(example))
-        println(Day12.solve2(puzzleInput))
     }
 }
